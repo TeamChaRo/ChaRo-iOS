91bdaa37cffe0dfd60f4919b01516292da01fcaa
indoni <choinj97@gmail.com>
2021-07-02 00:20:59 +0900

    ➕ [ADD] #9 - PostDetailVC

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/PostDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  PostDetailVC.swift
+//  ChaRo-iOS
+//
+//  Created by 최인정 on 2021/07/02.
+//
+
+import UIKit
+
+class PostDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
