58e819e2ca38bcc5e77dceb8d7ff849f4711dcd6
parkikbum <pibum@naver.com>
2021-07-02 15:55:05 +0900

    ✨ [FEAT] 애니메이션 컬렉션 뷰 레이아웃 잡기

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/HomeAnimationTVC.swift
@@ -0,0 +1,23 @@
+//
+//  HomeAnimationTVC.swift
+//  ChaRo-iOS
+//
+//  Created by 박익범 on 2021/07/02.
+//
+
+import UIKit
+
+class HomeAnimationTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
