e39b4a75e182f50f650926c6fcf5c58c85f64b1b
parkikbum <pibum@naver.com>
2021-07-03 00:46:36 +0900

    ✨ [FEAT] #12 메인 뷰 애니메이션 구현 일단 완료

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/HomeAnimationCVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/HomeAnimationCVC.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class HomeAnimationCVC: UICollectionViewCell {
+    
+    
+    static let identifier = "HomeAnimationCVC"
+    
     @IBOutlet weak var roadImage: UIImageView!
     @IBOutlet weak var carImage: UIImageView!
+    @IBOutlet weak var homeAnimationTitleLabel: UILabel!
+    @IBOutlet weak var homeAnimationHashtagLabel: UILabel!
+    @IBOutlet weak var backGroundImage: UIImageView!
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    func setConsraintCar(){
+        
+    }
+   
+    func setData(imageName : String, titleText : String, hashTagText : String){
+        if let userImage = UIImage(named: imageName){
+            backGroundImage.image = userImage
+        }
+        homeAnimationTitleLabel.text = titleText
+        homeAnimationHashtagLabel.text = hashTagText
+    }
+    
 }
