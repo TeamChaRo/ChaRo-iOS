5c0c824c0649549807f075c83ca916b1be0684c6
parkikbum <pibum@naver.com>
2021-07-02 15:55:05 +0900

    ✨ [FEAT] 애니메이션 컬렉션 뷰 레이아웃 잡기

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/HomeAnimationCVC.swift
@@ -0,0 +1,19 @@
+//
+//  HomeAnimationCVC.swift
+//  ChaRo-iOS
+//
+//  Created by 박익범 on 2021/07/02.
+//
+
+import UIKit
+
+class HomeAnimationCVC: UICollectionViewCell {
+    @IBOutlet weak var roadImage: UIImageView!
+    @IBOutlet weak var carImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
