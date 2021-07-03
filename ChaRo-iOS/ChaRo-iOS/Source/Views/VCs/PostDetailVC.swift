cb5fe320376403e59e88ea4cfc854830c31bd9c4
hryeong66 <jhr110326@gmail.com>
2021-07-03 19:15:23 +0900

    ✅[CHORE] : #17 - post parking TVC 테스트 코드 추가

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/PostDetailVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/PostDetailVC.swift
@@ -14,17 +14,42 @@ class PostDetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureTableView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureTableView(){
+        registerXibs()
+        postDetailTableView.delegate = self
+        postDetailTableView.dataSource = self
     }
-    */
-
+    
+    private func registerXibs(){
+        postDetailTableView.registerCustomXib(xibName: PostParkingTVC.identifier)
+    }
 }
+
+//MARK:- TableView Delegate
+extension PostDetailVC: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 151
+    }
+}
+
+extension PostDetailVC: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PostParkingTVC.identifier) as? PostParkingTVC else {
+            return UITableViewCell()
+        }
+        
+        cell.setParkingStatus(status: true)
+        cell.setParkingExplanation(text: "100m 이내에 주차공간이 있었어요 :)")
+        return cell
+    }
+    
+    
+}
+
