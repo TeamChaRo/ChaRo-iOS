5e75a394db5d20d2a5bfc63c255238a9670a7eb3
Jen <wonee0321@gmail.com>
2021-07-04 23:33:53 +0900

    ✨ [FEAT]CommonCVC 구현

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/CVC/CommonCVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/CVC/CommonCVC.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class CommonCVC: UICollectionViewCell {
 
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var tagView: UIView!
+    @IBOutlet weak var tagLabel: UILabel!
+    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         
