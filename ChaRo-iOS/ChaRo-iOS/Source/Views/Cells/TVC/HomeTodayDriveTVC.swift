f7cf37222753221f3af54393df2961f8b1dfcc6f
Jen <wonee0321@gmail.com>
2021-07-04 23:09:11 +0900

    ✨ [FEAT]HomeTodayDriveTVC 구현

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/HomeTodayDriveTVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/HomeTodayDriveTVC.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class HomeTodayDriveTVC: UITableViewCell {
 
+    
+    //MARK:- IBOutlet
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    //MARK:- Variable
+    static let identifier = "TodayDriveTVC"
+    
+    
+    //MARK:- Life Cycle
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+        setCollctionView()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -19,5 +29,60 @@ class HomeTodayDriveTVC: UITableViewCell {
 
         
     }
+    
+    //MARK:- default Setting Function Part
+    func setCollctionView() {
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerCustomXib(xibName: "TodayDriveCVC")
+    
+    }
+    
+    //MARK:- Function
+    
+}
+
+
+//MARK:- extension
+
+extension HomeTodayDriveTVC: UICollectionViewDelegate ,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CommonCVC", for: indexPath) as? CommonCVC else { return UICollectionViewCell() }
+    
+        //cell.imageView.image = UIImage(named: "tempImageBig")
+        return cell
+        
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        //나중에 바꿀예정 ..
+        return CGSize(width: 260, height: 256)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        if section == 0 {
+            return UIEdgeInsets(top: 0, left: 11, bottom: 0, right: 0)
+        }
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        
+        
+    }
+    
     
 }
