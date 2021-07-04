2721222ca28778f51ac5b79460b39746ab8702f2
Jen <wonee0321@gmail.com>
2021-07-04 23:15:15 +0900

    ✨ [FEAT]HomeSquareTVC 구현

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/HomeSquareTVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/HomeSquareTVC.swift
@@ -9,15 +9,79 @@ import UIKit
 
 class HomeSquareTVC: UITableViewCell {
 
+    //MARK:- IBOutlet
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    
+    //MARK:- Variable
+    static let identifier = "HomeSquareTVC"
+    
+    
+    //MARK:- Life Cycle
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setCollctionView()
     }
-
+    
+    //MARK:- default Setting Function Part
+    func setCollctionView() {
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.registerCustomXib(xibName: "HomeSquareTVC")
+        
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
+        
     }
+    
+    //MARK:- Function
+    
+}
+
+//MARK:- extension
+
+extension HomeSquareTVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CommonCVC", for: indexPath) as? CommonCVC else { return UICollectionViewCell() }
+        
+        //cell.imageView.image = UIImage(named: "tempImageSmall")
+        
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        //일단요 ... 고정..
+        let width = collectionView.frame.width / 2 - 20
+        
+        let size = CGSize(width: width, height: 250)
+        
+        return size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    } //옆
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 25
+    } //위아래
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
+    
+    
     
 }
