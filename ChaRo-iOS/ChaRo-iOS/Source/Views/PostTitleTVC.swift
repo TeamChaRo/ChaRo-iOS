db04d584cd50d969c82a8be4f931f0a61c5212a4
indoni <choinj97@gmail.com>
2021-07-04 21:30:25 +0900

    ✨[FEAT] : #11 - post datail view 상단부 xib셋팅 및 autolayout 적용완료"

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/PostTitleTVC.swift
@@ -0,0 +1,22 @@
+//
+//  PostTitleTVC.swift
+//  ChaRo-iOS
+//
+//  Created by 최인정 on 2021/07/03.
+//
+
+import UIKit
+
+class PostTitleTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
