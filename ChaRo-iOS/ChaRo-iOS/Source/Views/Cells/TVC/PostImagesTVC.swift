9c1acb24344425d0d00c650eddd0d547892d08da
indoni <choinj97@gmail.com>
2021-07-06 01:38:06 +0900

    ➕[ADD] : #26 - postDetail imageView TVC and xib 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/PostImagesTVC.swift
@@ -0,0 +1,23 @@
+//
+//  PostImagesTVC.swift
+//  ChaRo-iOS
+//
+//  Created by 최인정 on 2021/07/06.
+//
+
+import UIKit
+
+class PostImagesTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
