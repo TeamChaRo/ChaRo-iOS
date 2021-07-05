f125618b4decff80d962dc23e963270581d013f5
indoni <choinj97@gmail.com>
2021-07-06 04:43:15 +0900

    ✨[FEAT]: #26 - PostDetail view image scroll view 구현완료

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/PostImagesTVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/PostImagesTVC.swift
@@ -8,16 +8,127 @@
 import UIKit
 
 class PostImagesTVC: UITableViewCell {
+    
+    static let identifier: String = "PostImagesTVC"
 
+    // MARK: - Outlet Variables
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var previousButton: UIButton!
+    @IBOutlet weak var pageController: UIPageControl!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    // MARK: - images
+    // ToDo: - 일단 지금은 더미데이터로 넣고, 추후 서버 통신으로 리스트 append 예정
+    var images: [String] = []
+    var postImageView = [UIImageView]()
+    let imageHeightRate: CGFloat = 222 / 815
+    let ImageViewWidth: CGFloat = UIScreen.main.bounds.width
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        configureScrollView()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+    }
+    
+    private func configureScrollView(){
+        scrollView.delegate = self
+    }
+    
+    // MARK: - IBActions
+    @IBAction func pageChanged(_ sender: Any) {
+        setButton()
+    }
+    
+    @IBAction func nextButtonDidTap(_ sender: Any) {
+        if pageController.currentPage < images.count-1 {
+            UIImageView.animate(withDuration: 0.4, animations: { [self] in
+                self.scrollView.contentOffset.x += UIScreen.main.bounds.width
+            })
+            let value = scrollView.contentOffset.x/scrollView.frame.size.width
+            setPageControlSelectedPage(currentPage: Int(round(value)))
+            setButton()
+        }
+    }
+    
+    @IBAction func previousButtonDidTap(_ sender: Any) {
+        if pageController.currentPage > 0 {
+            UIImageView.animate(withDuration: 0.4, animations: {
+                self.scrollView.contentOffset.x -= UIScreen.main.bounds.width
+            })
+            let value = scrollView.contentOffset.x/scrollView.frame.size.width
+            setPageControlSelectedPage(currentPage: Int(round(value)))
+            setButton()
+        }
+    }
+    
+}
+
+extension PostImagesTVC: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let value = scrollView.contentOffset.x/scrollView.frame.size.width
+        setPageControlSelectedPage(currentPage: Int(round(value)))
+    }
+}
+
+// MARK: - functions
+extension PostImagesTVC {
+
+    func setImage(_ imgNames: [String]){
+        images = imgNames
+        initImagePage()
+        initScrollView()
+        addImageView()
+    }
+    
+    func initImagePage(){
+        pageController.numberOfPages = images.count
+        pageController.currentPage = 0 // 시작은 첫 페이지
+        pageController.isHidden = false // 나중에 true로 바꾸기!!
+        setButton()
+    }
+    
+    func initScrollView(){
+        scrollView.frame = UIScreen.main.bounds
+        scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width * CGFloat(images.count), height: UIScreen.main.bounds.height * imageHeightRate)
+        scrollView.alwaysBounceVertical = true
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.isPagingEnabled = true
+        scrollView.bounces = false
+    }
+    
+    func setPageControlSelectedPage(currentPage:Int) {
+        pageController.currentPage = currentPage
+        setButton()
+    }
+    
+    func addImageView(){
+        for i in 0..<images.count{
+            let imageView = UIImageView()
+            let xPos = UIScreen.main.bounds.width * CGFloat(i)
+            imageView.frame = CGRect(x: xPos, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * imageHeightRate)
+            imageView.contentMode = .scaleAspectFill
+            imageView.image = UIImage(named: images[i])
+            scrollView.addSubview(imageView)
+            scrollView.contentSize.width = imageView.frame.width * CGFloat(i+1)
+        }
+    }
+    
+    func setButton(){
+        if pageController.currentPage == images.count-1 { // 마지막 페이지
+            nextButton.isHidden = true
+            previousButton.isHidden = false
+        } else if pageController.currentPage == 0 { // 첫 페이지
+            nextButton.isHidden = false
+            previousButton.isHidden = true
+        } else { // 그 외 페이지
+            nextButton.isHidden = false
+            previousButton.isHidden = false
+        }
     }
     
 }
