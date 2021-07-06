e3837b09df3766708573aba29bc2bd4b12c4a621
indoni <choinj97@gmail.com>
2021-07-07 02:57:48 +0900

    ➕[ADD]: #37 - courseTheme xib cell파일 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/PostCourseThemeTVC.swift
@@ -0,0 +1,23 @@
+//
+//  PostCourseThemeTVC.swift
+//  ChaRo-iOS
+//
+//  Created by 최인정 on 2021/07/07.
+//
+
+import UIKit
+
+class PostCourseThemeTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
