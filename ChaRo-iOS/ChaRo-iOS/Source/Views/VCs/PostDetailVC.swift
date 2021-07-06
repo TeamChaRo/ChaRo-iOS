8f1d9c8253f60c18cc919c8960007de48aaa8f69
hryeong66 <jhr110326@gmail.com>
2021-07-06 18:11:05 +0900

    ✅[CHORE] : #28 - post drive cource cell 추가

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/PostDetailVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/PostDetailVC.swift
@@ -33,6 +33,8 @@ class PostDetailVC: UIViewController {
         postDetailTableView.registerCustomXib(xibName: PostTitleTVC.identifier)
         postDetailTableView.registerCustomXib(xibName: PostParkingTVC.identifier)
         postDetailTableView.registerCustomXib(xibName: PostAttentionTVC.identifier)
+        postDetailTableView.registerCustomXib(xibName: PostDriveCourseTVC.identifier)
+        
     }
     
 }
@@ -49,6 +51,10 @@ extension PostDetailVC: UITableViewDelegate{
             return 159
         case 2:
             return 159
+        case 3:
+            return 408
+        case 4:
+            return 418
         default:
             return 159
         }
@@ -57,9 +63,8 @@ extension PostDetailVC: UITableViewDelegate{
 
 extension PostDetailVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return 5
     }
-    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
@@ -70,6 +75,10 @@ extension PostDetailVC: UITableViewDataSource {
             return getPostParkingCell(tableView: tableView)
         case 2:
             return getPostAttensionCell(tableView: tableView)
+        case 3:
+            return getPostDriveCourceCell(tableView: tableView)
+        case 4:
+            return getPostDriveCourceCell(tableView: tableView, test: true)
         default:
             return getPostAttensionCell(tableView: tableView)
         }
@@ -92,10 +101,22 @@ extension PostDetailVC: UITableViewDataSource {
     func getPostParkingCell(tableView: UITableView) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: PostParkingTVC.identifier) as? PostParkingTVC else {return UITableViewCell()}
         
-        //cell.setParkingStatus(isParking: true)
         cell.setParkingStatus(status: true)
         cell.setParkingExplanation(text: "100m 이내에 주차공간이 있었어요 :)")
         cell.idEditMode(isEditing: false)
+        return cell
+    }
+    
+    func getPostDriveCourceCell(tableView: UITableView) -> UITableViewCell{
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PostDriveCourseTVC.identifier) as? PostDriveCourseTVC else {return UITableViewCell()}
+        
+        cell.setContentText(text: "안녕하세요. 이제 점점 봄이 오나봐요. 아파트 내에 목련과 개나리가 피었더라구요. 코로나19때문에 집콕하는 동안에도, 어김없이 봄과함께 벚꽃축제 시즌이 우리 곁에 왔나봅니다. 가족과 서울 금천 벚꽃 십리길에 드라이브를 갔다왔어요. 길게 뻗은 벚꽃 길의 길이가 3.7Km로 약10리에 달한다고 해요. 딸이 이 얘길 듣고 어찌나 놀라던지. 철도길과 함께 뻗은 길을 따라 드라이브 하면 흩날리는 꽃 비를 즐길 수도 있다고 하네요. 모두 봄의 내음을 듬뿍 받을 수 있는 금천 벚꽃 십리길에 다녀와보세요~:)")
+        return cell
+    }
+
+    func getPostDriveCourceCell(tableView: UITableView, test: Bool) -> UITableViewCell{
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PostDriveCourseTVC.identifier) as? PostDriveCourseTVC else {return UITableViewCell()}
+        cell.setContentText(text: "")
         return cell
     }
 }
