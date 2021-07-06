f8823eb7d13de3af89660f00ccc6b01e5951e1d9
Jen <wonee0321@gmail.com>
2021-07-07 04:02:47 +0900

    ➕[ADD] : #39 template 추가

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/ThemePostTVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/ThemePostTVC.swift
@@ -9,15 +9,30 @@ import UIKit
 
 class ThemePostTVC: UITableViewCell {
 
+    //MARK:- IBOutlet
+   
+    
+    //MARK:- Variable
+    static let identifier = "ThemePostTVC"
+
+    
+    //MARK:- Life Cycle
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
     }
     
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+    //MARK:- default Setting Function Part
+    
+    
+    //MARK:- Function
+    
 }
+
+//MARK:- extension
+
