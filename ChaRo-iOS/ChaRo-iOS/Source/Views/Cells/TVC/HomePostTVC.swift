bcf144a9c8e822bbc0815daf8d66b5e0c3e0e52f
parkikbum <pibum@naver.com>
2021-07-07 04:42:11 +0900

    ➕[ADD]: #35 - 테이블뷰 셀 및 컬렉션뷰 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/HomePostTVC.swift
@@ -0,0 +1,23 @@
+//
+//  HomePostTVC.swift
+//  ChaRo-iOS
+//
+//  Created by 박익범 on 2021/07/07.
+//
+
+import UIKit
+
+class HomePostTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
