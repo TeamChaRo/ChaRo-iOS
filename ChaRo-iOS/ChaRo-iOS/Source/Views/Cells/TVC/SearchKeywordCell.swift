f26cbe3f39064ced06396b8e4d0877f6ada85a2d
hryeong66 <jhr110326@gmail.com>
2021-07-07 16:43:23 +0900

    ✅[CHORE] : #34 - 검색결과 보여줄 cell title, address 추가

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/SearchKeywordCell.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/SearchKeywordCell.swift
@@ -9,15 +9,47 @@ import UIKit
 
 class SearchKeywordCell: UITableViewCell {
 
+    static let identifier = "SearchKeywordCell"
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        //style
+        return label
+    }()
+    
+    private var addressLabel: UILabel = {
+        let label = UILabel()
+        //style
+        return label
+    }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setConstraints()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+    }
+    
+    private func setConstraints(){
+        addSubviews([titleLabel,
+                     addressLabel])
+        
+        titleLabel.snp.makeConstraints{
+            $0.top.equalTo(self.snp.top).offset(13)
+            $0.leading.equalTo(self.snp.leading).offset(20)
+        }
+        
+        addressLabel.snp.makeConstraints{
+            $0.top.equalTo(titleLabel.snp.bottom).offset(2)
+            $0.leading.equalTo(self.snp.leading).offset(20)
+        }
+    }
+    
+    public func setContents(title: String, address: String){
+        titleLabel.text = title
+        addressLabel.text = address
     }
     
 }
