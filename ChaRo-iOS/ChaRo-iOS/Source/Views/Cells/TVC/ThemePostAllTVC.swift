d6716f058ca1f9519b1e7011798ace2214e1e90e
Jen <wonee0321@gmail.com>
2021-07-08 05:33:42 +0900

    ✨[FEAT] : ThemePostAllTVC 구현

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/ThemePostAllTVC.swift
@@ -0,0 +1,23 @@
+//
+//  ThemePostAllTVC.swift
+//  ChaRo-iOS
+//
+//  Created by JEN Lee on 2021/07/08.
+//
+
+import UIKit
+
+class ThemePostAllTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
