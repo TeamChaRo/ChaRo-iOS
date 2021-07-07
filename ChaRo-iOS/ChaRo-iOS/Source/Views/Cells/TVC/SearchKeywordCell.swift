08463854a7d644ae09d9c2aa2afb2513ccd7313a
hryeong66 <jhr110326@gmail.com>
2021-07-07 16:31:59 +0900

    ➕[ADD] : #34 - keyword 검색 보여줄 cell 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/SearchKeywordCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchKeywordCell.swift
+//  ChaRo-iOS
+//
+//  Created by 장혜령 on 2021/07/07.
+//
+
+import UIKit
+
+class SearchKeywordCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
