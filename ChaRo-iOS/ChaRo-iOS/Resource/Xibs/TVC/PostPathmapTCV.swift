424890edac5a0dcdd5046c22fd0c5f732b66dcde
indoni <choinj97@gmail.com>
2021-07-09 01:58:07 +0900

    ✅[CHORE]: #43 - TMapView setting 코드 추가

--- ChaRo-iOS/ChaRo-iOS/Resource/Xibs/TVC/PostPathmapTCV.swift
+++ ChaRo-iOS/ChaRo-iOS/Resource/Xibs/TVC/PostPathmapTCV.swift
@@ -6,18 +6,51 @@
 //
 
 import UIKit
+import SnapKit
+import TMapSDK
 
 class PostPathmapTCV: UITableViewCell {
 
+    static let identifier: String = "PostPathmapTCV"
+    
+    var postMap = MapService.getTmapView()
+    
+    let multiplier: CGFloat = 395/335
+    let mapWidth: CGFloat = UIScreen.main.bounds.width - 40
+    
+    let mapViewContainerView: UIView = UIView()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
     
 }
+
+extension PostPathmapTCV {
+
+    func configureLayout(){
+        addSubview(mapViewContainerView)
+        
+        mapViewContainerView.snp.makeConstraints{
+            $0.top.equalTo(self.snp.top)
+            $0.leading.equalTo(self.snp.top)
+            $0.trailing.equalTo(self.snp.top)
+            $0.bottom.equalTo(self.snp.top)
+            $0.width.equalTo(postMap)
+            $0.height.equalTo(self.mapViewContainerView.snp.width).multipliedBy(multiplier)
+        }
+        
+        setMapView()
+    }
+    
+    func setMapView(){
+        mapViewContainerView.addSubview(postMap)
+        postMap.frame = mapViewContainerView.frame
+    }
+    
+}
+
