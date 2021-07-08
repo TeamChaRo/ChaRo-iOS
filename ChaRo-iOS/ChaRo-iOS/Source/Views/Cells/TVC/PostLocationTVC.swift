431f250caba99c1db67c43753583b68f5067b6bb
indoni <choinj97@gmail.com>
2021-07-09 00:36:19 +0900

    💄[MOD]: copy button asset 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/PostLocationTVC.swift
@@ -0,0 +1,23 @@
+//
+//  PostLocationTVC.swift
+//  ChaRo-iOS
+//
+//  Created by 최인정 on 2021/07/08.
+//
+
+import UIKit
+
+class PostLocationTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
