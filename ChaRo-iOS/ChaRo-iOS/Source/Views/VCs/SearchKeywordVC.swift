b2bda68c996424a9d9afe6b0c7a44e8cda191bb5
hryeong66 <jhr110326@gmail.com>
2021-07-09 01:24:23 +0900

    ➕[ADD] : #42 - mapview 연결해서 센터 이동 구현

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SearchKeywordVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SearchKeywordVC.swift
@@ -7,15 +7,19 @@
 
 import UIKit
 import SnapKit
+import TMapSDK
 
 class SearchKeywordVC: UIViewController {
 
     static let identifier = "SearchKeywordVC"
     public var keyword = "안녕하세요"
+    private let mapView = MapService.getTmapView()
     private var addressIndex = -1
+    private var addressType = ""
     private var resultAddress : AddressDataModel?
     private var keywordTableView = UITableView()
     private var autoCompletedKeywordList : [AddressDataModel] = []
+    private var autoList : [String] = []
     
     private var backButton: UIButton = {
         let button = UIButton()
@@ -28,6 +32,7 @@ class SearchKeywordVC: UIViewController {
         textField.autocorrectionType = .no
         textField.font = .notoSansRegularFont(ofSize: 17)
         textField.textColor = .gray50
+        textField.clearsOnBeginEditing = true
         return textField
     }()
     
@@ -41,6 +46,8 @@ class SearchKeywordVC: UIViewController {
         super.viewDidLoad()
         setConstraints()
         setActionToComponent()
+        configureTableView()
+        navigationController?.isNavigationBarHidden = true
     }
     
     private func setConstraints(){
@@ -55,6 +62,7 @@ class SearchKeywordVC: UIViewController {
             $0.height.equalTo(48)
             $0.width.equalTo(48)
         }
+        
         searchTextField.snp.makeConstraints{
             $0.centerY.equalTo(backButton.snp.centerY)
             $0.leading.equalTo(backButton.snp.trailing)
@@ -76,25 +84,86 @@ class SearchKeywordVC: UIViewController {
         
     }
     
-    private func setResultAddress(){
-        resultAddress = AddressDataModel(latitude: 10.0, longtitude: 10.0, address: "연남동 합숙자리")
-    }
-    
-    public func setAddressModel(model: AddressDataModel, text: String, index: Int){
+    public func setAddressModel(model: AddressDataModel, cellType: String, index: Int){
         resultAddress = model
-        searchTextField.placeholder = text
+        searchTextField.placeholder = "\(cellType)를 입력해주세요"
+        addressType = cellType
         addressIndex = index
-        
     }
     
     public func setActionToComponent(){
         backButton.addTarget(self, action: #selector(dismissAction), for: .touchUpInside)
+        searchTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+    }
+    
+    private func getAddressConfirmVC() -> AddressConfirmVC{
+        let storyboard = UIStoryboard(name: "AddressConfirm", bundle: nil)
+        guard let nextVC = storyboard.instantiateViewController(identifier: AddressConfirmVC.identifier) as? AddressConfirmVC else {
+            return AddressConfirmVC()
+        }
+        return nextVC
+    }
+    
+    private func reloadTableViewData(){
+        print("reloadTableViewData")
+        DispatchQueue.main.sync {
+            print("count = \(self.autoCompletedKeywordList.count) ")
+            self.keywordTableView.reloadData()
+        }
     }
     
     @objc func dismissAction(){
         let beforeVC = presentingViewController as! TabbarVC
         beforeVC.addressMainVC?.addressCellList[addressIndex].setAddressText(addressText: searchTextField.text!)
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    private func searchKeywordPOI(keywordList: [String], pathData: TMapPathData){
+        var results = 0
+        var isLoaded = false
+        print("keywordList = \(keywordList.count)")
+        autoCompletedKeywordList.removeAll()
+        DispatchQueue.global().sync {
+            for keyword in keywordList{
+                print(keyword)
+                pathData.requestFindAllPOI(keyword, count: 5){ poiResults, error in
+                    guard let poiItemList = poiResults else{ return }
+                    for poi in poiItemList{
+                        if results >= 10 {
+                            if !isLoaded{
+                                print("results is 12")
+                                isLoaded = true
+                                self.reloadTableViewData()
+                            }
+                            break
+                        }
+                        let poiItem = AddressDataModel(latitude: poi.coordinate?.latitude ?? 0.0,
+                                                       longtitude: poi.coordinate?.longitude ?? 0.0,
+                                                       address: poi.address ?? "주소안뜸",
+                                                       title: poi.name ?? "이름없음")
+                        self.autoCompletedKeywordList.append(poiItem)
+                        results += 1
+                        print(results)
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        let pathData = TMapPathData()
+        
+        print("search Keyword = \(textField.text)")
+        let searchKeyword = textField.text!
+       // var autoKeywordList : [String] = []
+        DispatchQueue.global().sync {
+            pathData.autoComplete(searchKeyword){results , error in
+                //autoKeywordList = results
+                self.searchKeywordPOI(keywordList: results, pathData: pathData)
+            }
+        }
     }
 }
 
@@ -109,10 +178,8 @@ extension SearchKeywordVC {
     }
     
     func setTableViewStyle(){
-        
     }
 
-    
 }
 
 extension SearchKeywordVC: UITableViewDelegate{
@@ -128,8 +195,23 @@ extension SearchKeywordVC: UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchKeywordCell.identifier) as? SearchKeywordCell else {return UITableViewCell()}
+        
+        if autoCompletedKeywordList.count == 0 {
+            return UITableViewCell()
+        }
+        
         let address = autoCompletedKeywordList[indexPath.row]
-        cell.setContents(title: address.title, address: address.address)
+        cell.setContents(addressMadel: address)
+        //cell.selectionStyle = .none
+        cell.presentingMapViewClosure = { address in
+            let nextVC = self.getAddressConfirmVC()
+            nextVC.setPresentingAddress(address: address)
+            nextVC.setSearchType(type: self.addressType)
+            self.navigationController?.pushViewController(nextVC, animated: true)
+        }
+        
+        // cell.setContents(title: address.title, address: address.address)
+        //cell.setContents(title: autoList[indexPath.row], address: autoList[indexPath.row])
         return cell
     }
 }
@@ -143,4 +225,5 @@ extension SearchKeywordVC : UITextFieldDelegate{
         print(keyword)
     }
     
+  
 }
