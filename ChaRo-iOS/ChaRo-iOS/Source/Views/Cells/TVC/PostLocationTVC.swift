b915a977964ce08bedfabe30a44f4214f3aafb31
indoni <choinj97@gmail.com>
2021-07-09 23:42:13 +0900

    ✅[CHORE]: #38 - 코드 일부 수정

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/PostLocationTVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/PostLocationTVC.swift
@@ -22,14 +22,15 @@ class PostLocationTVC: UITableViewCell {
     
     let copyButton: UIButton = {
         let button = UIButton()
-        button.setBackgroundImage(UIImage(named: "copy1_light_ver"), for: .normal)
+        button.setBackgroundImage(UIImage(named: "copyLightVer"), for: .normal)
         button.imageView?.contentMode = .scaleAspectFill
         return button
     }()
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        configureLayout()
     }
     
     
@@ -53,7 +54,7 @@ extension PostLocationTVC {
         addSubviews([titleView, locationTextField, copyButton])
         
         titleView.snp.makeConstraints{
-            $0.top.equalTo(self.snp.top)
+            $0.top.equalTo(self.snp.top).offset(10)
             $0.leading.equalTo(20)
             $0.width.equalTo(50)
         }
@@ -61,6 +62,16 @@ extension PostLocationTVC {
         locationTextField.snp.makeConstraints{
             $0.top.equalTo(self.snp.top)
             $0.leading.equalTo(self.titleView.snp.trailing).offset(3)
+            $0.height.equalTo(42)
+            $0.bottom.equalTo(self.snp.bottom).offset(8)
+            
+        }
+        
+        copyButton.snp.makeConstraints{
+            $0.top.equalTo(self.snp.top)
+            $0.leading.equalTo(self.locationTextField.snp.trailing)
+            $0.bottom.equalTo(self.snp.bottom).inset(8)
+            $0.trailing.equalTo(self.snp.trailing).inset(6)
         }
         
     }
