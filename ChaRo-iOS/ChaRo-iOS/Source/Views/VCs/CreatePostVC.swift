14c2e19210cdfe5a9a7f16fb5a1f53cb91faad7d
indoni <choinj97@gmail.com>
2021-07-11 03:40:55 +0900

    ➕[ADD]: #51 - 작성하기 sroryboard, VC 파일 생성 및 연결

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/CreatePostVC.swift
@@ -0,0 +1,17 @@
+//
+//  CreatePostVC.swift
+//  ChaRo-iOS
+//
+//  Created by 최인정 on 2021/07/11.
+//
+
+import UIKit
+
+class CreatePostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
