83fd3cd8b4287f6d7872d8cb914e1b4a92238d5b
hryeong66 <jhr110326@gmail.com>
2021-07-11 18:42:10 +0900

    ➕[ADD] : #54 - 검색 결과 나타날 뷰 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SearchResultVC.swift
@@ -0,0 +1,26 @@
+//
+//  SearchResultVC.swift
+//  ChaRo-iOS
+//
+//  Created by 장혜령 on 2021/07/11.
+//
+
+import UIKit
+
+class SearchResultVC: UIViewController {
+
+    static let identifier = "SearchResultVC"
+    
+    private let navigationView = UIView()
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
