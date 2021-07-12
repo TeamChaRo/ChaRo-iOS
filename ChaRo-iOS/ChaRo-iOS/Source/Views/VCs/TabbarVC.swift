fdc6be0069e54a4afe072d375d9ba02efa7f5534
indoni <choinj97@gmail.com>
2021-07-12 23:08:47 +0900

    🔨[FIX]: #60 - solved merge conflict

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/TabbarVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/TabbarVC.swift
@@ -59,11 +59,8 @@ class TabbarVC: UITabBarController {
         homeTab.tabBarItem = UITabBarItem(title: "구경하기", image: UIImage(named: "tabbarIcHomeInactive"), selectedImage: UIImage(named: "tabbarIcHomeActive"))
        
         
-<<<<<<< HEAD
         
 
-=======
->>>>>>> develop
         let createStoryboard = UIStoryboard(name: "CreatePost", bundle: nil)
 
         let createVC = createStoryboard.instantiateViewController(identifier: CreatePostVC.identifier)
@@ -72,43 +69,15 @@ class TabbarVC: UITabBarController {
         createTab.tabBarItem = UITabBarItem(title: "작성하기", image: UIImage(named: "tabbarIcPostWrite"), selectedImage: UIImage(named: "tabbarIcPostWrite"))
         createTab.tabBarItem.imageInsets = UIEdgeInsets(top: -13, left: 0, bottom: 5, right: 0)
         
-<<<<<<< HEAD
 
         
-=======
->>>>>>> develop
         
         let myPageStoryboard = UIStoryboard(name: "MyPage", bundle: nil)
         let myPageTab = myPageStoryboard.instantiateViewController(identifier: "MyPageVC")
         myPageTab.tabBarItem = UITabBarItem(title: "나의차로", image: UIImage(named: "tabbarIcMypageInactive"), selectedImage: UIImage(named: "tabbarIcMypageActive"))
         
 
-<<<<<<< HEAD
-//
-//        mapTab.tabBarItem.image = UIImage(named: "tabbarIcPostWrite")
-//        mapTab.tabBarItem.title = "작성하기"
-//        mapTab.tabBarItem.imageInsets = UIEdgeInsets(top: -13, left: 0, bottom: 5, right: 0)
-//
-        
-//        let mapStoryboard = UIStoryboard(name: "AddressMain", bundle: nil)
-//        let mapTab = mapStoryboard.instantiateViewController(identifier: AddressMainVC.identifier) as! AddressMainVC
-//        addressMainVC = mapTab
-//
-//        mapTab.tabBarItem.image = UIImage(named: "tabbarIcPostWrite")
-//        mapTab.tabBarItem.title = "작성하기"
-//        mapTab.tabBarItem.imageInsets = UIEdgeInsets(top: -13, left: 0, bottom: 5, right: 0)
-//
-        
-//        let writtingTab = postStoryboard.instantiateViewController(identifier: "PostDetailVC")
-//        writtingTab.tabBarItem.image = UIImage(named: "tabbarIcPostWrite")
-//        writtingTab.tabBarItem.title = "작성하기"
-//        writtingTab.tabBarItem.imageInsets = UIEdgeInsets(top: -13, left: 0, bottom: 5, right: 0)
-        
-        let tabs = [homeTab, createTab, myPageTab]
-
-=======
         let tabs = [homeTab,createTab, myPageTab]
->>>>>>> develop
         
         setViewControllers(tabs, animated: true)
         selectedViewController = homeTab
