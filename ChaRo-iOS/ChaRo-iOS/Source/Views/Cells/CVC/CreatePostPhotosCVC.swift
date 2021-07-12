2810e9d05cafc927234dfc24521125ce49c9c846
indoni <choinj97@gmail.com>
2021-07-12 23:01:16 +0900

    ➕[ADD]: #60 - 작성하기 사진 cell 파일 추가 및 기본 셋팅

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/CVC/CreatePostPhotosCVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/CVC/CreatePostPhotosCVC.swift
@@ -9,9 +9,80 @@ import UIKit
 
 class CreatePostPhotosCVC: UICollectionViewCell {
 
+    static let identifier: String = "CreatePostPhotosCVC"
+    
+    // MARK: UIComponent
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "imagePlaceholder")
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 8
+        return imageView
+    }()
+    
+    let deleteButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(named: "imgDelete36px"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFill
+        return button
+    }()
+    
+    let plusView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 8
+        view.layer.borderColor = UIColor.gray20.cgColor
+        view.layer.borderWidth = 1
+        return view
+    }()
+    
+    let plusButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(named: "plus"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFill
+        return button
+    }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
+}
+
+extension CreatePostPhotosCVC {
+    func plusViewConfigureLayout(){
+        addSubviews([plusView, plusButton])
+        plusView.snp.makeConstraints{
+            $0.top.equalTo(self.snp.top)
+            $0.leading.equalTo(self.snp.leading)
+            $0.trailing.equalTo(self.snp.trailing)
+            $0.height.equalTo(plusView.snp.width).multipliedBy(1) // 1:1 ratio
+        }
+        
+        plusButton.snp.makeConstraints{
+            $0.top.equalTo(plusView.snp.top).offset(30)
+            $0.leading.equalTo(plusView.snp.leading).offset(30)
+            $0.trailing.equalTo(plusView.snp.trailing).inset(29)
+            $0.bottom.equalTo(plusView.snp.bottom).inset(29)
+            $0.width.equalTo(plusButton.snp.height).multipliedBy(1)
+        }
+    }
+    
+    func configureLayout(){
+        addSubviews([imageView, deleteButton])
+        imageView.snp.makeConstraints{
+            $0.top.equalTo(self.snp.top)
+            $0.leading.equalTo(self.snp.leading)
+            $0.trailing.equalTo(self.snp.trailing)
+            $0.height.equalTo(imageView.snp.width).multipliedBy(1)
+        }
+        
+        deleteButton.snp.makeConstraints{
+            let heightRatio: CGFloat = 36/107
+            $0.top.equalTo(imageView.snp.top)
+            $0.trailing.equalTo(imageView.snp.trailing)
+            $0.height.equalTo(imageView.snp.height).multipliedBy(heightRatio)
+            $0.width.equalTo(deleteButton.snp.height).multipliedBy(1)
+        }
+    }
 }
--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/CreatePostPhotoTVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/CreatePostPhotoTVC.swift
@@ -11,12 +11,80 @@ class CreatePostPhotoTVC: UITableViewCell {
 
     static let identifier: String = "CreatePostPhotoTVC"
     
+    // MARK: UI Components
+    let emptyImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "writeEmptyImage")
+        imageView.layer.cornerRadius = 8
+        return imageView
+    }()
+    
+    let collectionView: UICollectionView = UICollectionView()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+        selectionStyle = .none
+    }
+    
+}
+
+extension CreatePostPhotoTVC {
+    // MARK: Functions
+    func setCollcetionView(){
+        collectionView.delegate = self
+//        collectionView.dataSource = self
+        collectionView.registerCustomXib(xibName: CreatePostPhotosCVC.identifier)
+        
+        collectionView.showsHorizontalScrollIndicator = false
+    }
+    
+    // MARK: Layout
+    func emptyConfigureLayout(){
+        addSubview(emptyImageView)
+        
+        emptyImageView.snp.makeConstraints{
+            $0.top.equalTo(self.snp.top)
+            $0.leading.equalTo(self.snp.leading).offset(20)
+            $0.trailing.equalTo(self.snp.trailing).inset(20)
+            $0.bottom.equalTo(self.snp.bottom).inset(33)
+        }
+    }
+}
+
+extension CreatePostPhotoTVC : UICollectionViewDelegate {
+    
+}
+
+//extension CreatePostPhotoTVC : UICollectionViewDataSource {
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 10
+//    }
+//
+////    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+////        <#code#>
+////    }
+////
+////    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+////        <#code#>
+////    }
+//}
+
+extension CreatePostPhotoTVC : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 107, height: 107)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
     
 }
