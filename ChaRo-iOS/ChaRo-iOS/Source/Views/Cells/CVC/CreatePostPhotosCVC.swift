b3bcd9dd1ff16ea93062353d3a65891ad29a9edb
indoni <choinj97@gmail.com>
2021-07-12 22:58:27 +0900

    💄[MOD]: #60 - title, imageview에 필요한 asset 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/CVC/CreatePostPhotosCVC.swift
@@ -0,0 +1,17 @@
+//
+//  CreatePostPhotosCVC.swift
+//  ChaRo-iOS
+//
+//  Created by 최인정 on 2021/07/12.
+//
+
+import UIKit
+
+class CreatePostPhotosCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
