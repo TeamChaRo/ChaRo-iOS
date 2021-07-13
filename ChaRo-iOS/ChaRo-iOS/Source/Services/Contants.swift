bfc83866be01dde649bb61594d4339d59eb15763
hryeong66 <jhr110326@gmail.com>
2021-07-13 20:41:29 +0900

    ✅[CHORE] : #62 - searchKeyword get 안돼서 테스트

--- ChaRo-iOS/ChaRo-iOS/Source/Services/Contants.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Services/Contants.swift
@@ -13,8 +13,8 @@ struct Constants {
     static let baseURL = "http://3.139.62.132:5000"
     
     // MARK: - Feature URL
-    
     ///1. 홈 데이터 URL 여기에 + 유저 아이디(111,222,333) 해줘야 함 // 일단은 111로 해놓을겡~!
     static let HomeURL = baseURL + "/getMain/111"
     static let searchKeywordURL = baseURL + "/searchHistory"
+    static let getSearchKeywordURL = baseURL + "/searchHistory/111"
 }
