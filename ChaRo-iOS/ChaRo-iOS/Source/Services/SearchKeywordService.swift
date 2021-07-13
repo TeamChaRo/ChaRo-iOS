e94d1b78a08b42b83e12686c79abc9f1df2b18c8
hryeong66 <jhr110326@gmail.com>
2021-07-13 20:42:13 +0900

    ✅[CHORE] : #62 - search keyword get 추가

--- ChaRo-iOS/ChaRo-iOS/Source/Services/SearchKeywordService.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Services/SearchKeywordService.swift
@@ -13,32 +13,22 @@ import SwiftyJSON
 struct SearchKeywordService {
     
     static let shared = SearchKeywordService()
+    let header : HTTPHeaders = ["Content-Type" : "application/json"]
     
     private func makeParameter(userId: String, keywords: [SearchHistory]) -> Parameters {
-        let encoder = JSONEncoder()
-        
-        let jsonData = try! encoder.encode(keywords)
-        let jsonString = String(data: jsonData, encoding: .utf8)!
-        
-        return ["userId" : userId,
-                "searchHistory": jsonString]
+        let post = SearchKeywordDataModel(userId: userId, searchHistory: keywords)
+        return  post.toJSON()
     }
     
     func postSearchKeywords(userId: String,
-                                    keywords: [SearchHistory],
-                                    completion: @escaping (NetworkResult<Any>) -> Void){
+                            keywords: [SearchHistory],
+                            completion: @escaping (NetworkResult<Any>) -> Void){
         
-        let param = makeParameter(userId: userId, keywords: keywords)
-        print(param)
-        print("----------------------")
-        let header : HTTPHeaders = ["Content-Type" : "application/json"]
         let dataRequest = AF.request(Constants.searchKeywordURL,
                                      method: .post,
                                      parameters: makeParameter(userId: userId, keywords: keywords),
                                      encoding: JSONEncoding.default,
                                      headers: header)
-        
-        print("request 성공")
         
         dataRequest.responseData{ dataResponse in
             print("데이터 받기")
@@ -56,10 +46,32 @@ struct SearchKeywordService {
         }
     }
     
+    func getSearchKeywords(userId : String,
+                           completion: @escaping (NetworkResult<Any>) -> Void){
+       
+        let dataRequeat = AF.download(Constants.searchKeywordURL+"/111",
+                                      method: .get,
+                                      encoding: JSONEncoding.default,
+                                      headers: header)
+        
+        dataRequeat.responseData{ dataResponse in
+            switch dataResponse.result{
+            case .success:
+                guard let statusCode = dataResponse.response?.statusCode else { return}
+                guard let value = dataResponse.value  else {return}
+                let networkResult = self.judgeStatus(by: statusCode, value)
+                completion(networkResult)
+                
+            case .failure(_): completion(.pathErr)
+            }
+        }
+        
+    }
+    
     private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
         let decoder = JSONDecoder()
         
-        guard let decodeData = try? decoder.decode(SearchPostResultDataModel.self, from: data) else {
+        guard let decodeData = try? decoder.decode(SearchResultDataModel.self, from: data) else {
             return .pathErr
         }
         
@@ -71,5 +83,4 @@ struct SearchKeywordService {
             return .networkFail
         }
     }
-    
 }
