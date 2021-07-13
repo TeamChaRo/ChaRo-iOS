13f408114a68f2ca4834b1bbc2491e578e4ba3eb
indoni <choinj97@gmail.com>
2021-07-13 12:42:42 +0900

    ➕[ADD]: #60 - UI components 추가 완료

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/CreatePostCourseTVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/CreatePostCourseTVC.swift
@@ -11,6 +11,35 @@ class CreatePostCourseTVC: UITableViewCell {
 
     static let identifier: String = "CreatePostCourseTVC"
     
+    // MARK: UI Components
+    private let courseTitleView = PostCellTitleView(title: "어느 테마의 드라이브였나요?", subTitle: "드라이브 테마를 한 개 이상 선택해주세요.")
+    
+    private let cityButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    private let regionButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    private let cityLabel: UILabel = {
+        let label = UILabel()
+        label.text = "도 단위"
+        label.font = UIFont.notoSansMediumFont(ofSize: 14)
+        label.textColor = UIColor.gray40
+        return label
+    }()
+    
+    private let regionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "시 단위"
+        label.font = UIFont.notoSansMediumFont(ofSize: 14)
+        label.textColor = UIColor.gray40
+        return label
+    }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -22,4 +51,8 @@ class CreatePostCourseTVC: UITableViewCell {
         // Configure the view for the selected state
     }
     
+}
+
+extension CreatePostCourseTVC {
+    // MARK: Layout
 }
--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/CreatePostThemeTVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/CreatePostThemeTVC.swift
@@ -11,6 +11,48 @@ class CreatePostThemeTVC: UITableViewCell {
 
     static let identifier: String = "CreatePostThemeTVC"
     
+    // MARK: UI Components
+    private let courseTitleView = PostCellTitleView(title: "어느 지역으로 다녀오셨나요?", subTitle: "도/광역시, 시 단위로 선택해주세요.")
+
+    private let themeFirstButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    private let themeSecondButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    private let themeThirdButton: UIButton = {
+        let button = UIButton()
+        return button
+    }()
+    
+    private let themeFirstLabel: UILabel = {
+        let label = UILabel()
+        label.text = "테마 1"
+        label.font = UIFont.notoSansMediumFont(ofSize: 14)
+        label.textColor = UIColor.gray40
+        return label
+    }()
+    
+    private let themeSecondLabel: UILabel = {
+        let label = UILabel()
+        label.text = "테마 2"
+        label.font = UIFont.notoSansMediumFont(ofSize: 14)
+        label.textColor = UIColor.gray40
+        return label
+    }()
+    
+    private let themeThirdLabel: UILabel = {
+        let label = UILabel()
+        label.text = "테마 3"
+        label.font = UIFont.notoSansMediumFont(ofSize: 14)
+        label.textColor = UIColor.gray40
+        return label
+    }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -22,4 +64,8 @@ class CreatePostThemeTVC: UITableViewCell {
         // Configure the view for the selected state
     }
     
+}
+
+extension CreatePostThemeTVC{
+    // MARK: Layout
 }
