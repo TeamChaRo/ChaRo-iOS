79546879f9916bd9efa677b7088d8271948c6cbc
indoni <choinj97@gmail.com>
2021-07-13 12:24:49 +0900

    ➕[ADD]: #70 - 지역코스, 테마 TVC 파일 생성

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/CreatePostCourseTVC.swift
@@ -0,0 +1,25 @@
+//
+//  CreatePostCourseTVCTableViewCell.swift
+//  ChaRo-iOS
+//
+//  Created by 최인정 on 2021/07/13.
+//
+
+import UIKit
+
+class CreatePostCourseTVC: UITableViewCell {
+
+    static let identifier: String = "CreatePostCourseTVC"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/CreatePostThemeTVC.swift
@@ -0,0 +1,25 @@
+//
+//  CreatePostThemeTVC.swift
+//  ChaRo-iOS
+//
+//  Created by 최인정 on 2021/07/13.
+//
+
+import UIKit
+
+class CreatePostThemeTVC: UITableViewCell {
+
+    static let identifier: String = "CreatePostThemeTVC"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
