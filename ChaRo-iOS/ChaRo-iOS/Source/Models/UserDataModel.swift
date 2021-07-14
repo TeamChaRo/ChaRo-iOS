7fa9a88a0f14119cdd9fbd07149bbd3dad763636
Jen <wonee0321@gmail.com>
2021-07-15 05:53:49 +0900

    ➕[ADD] : #86 로그인 모델 구현

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Models/UserDataModel.swift
@@ -0,0 +1,33 @@
+//
+//  UserDataModel.swift
+//  ChaRo-iOS
+//
+//  Created by JEN Lee on 2021/07/15.
+//
+
+import Foundation
+
+// MARK: - UserDataModel
+struct UserDataModel: Codable {
+    let success: Bool
+    let msg: String
+    let data: User
+}
+
+// MARK: - User
+struct User: Codable {
+    let userID, nickname, token: String
+
+    enum CodingKeys: String, CodingKey {
+        case userID = "userId"
+        case nickname, token
+    }
+}
+
+
+
+//
+//`id` : `String` , 유저의 로그인 아이디
+//
+//`password` : `String` , 비밀번호
+//
