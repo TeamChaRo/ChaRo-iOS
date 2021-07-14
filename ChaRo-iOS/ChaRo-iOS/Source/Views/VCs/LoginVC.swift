1eb643a8deedf6b5e4aaa9bc2ac86da7b9a5bd4e
Jen <wonee0321@gmail.com>
2021-07-15 00:33:33 +0900

    ➕[ADD] : #86 LoginVC 생성, 연결

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/LoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginVC.swift
+//  ChaRo-iOS
+//
+//  Created by JEN Lee on 2021/07/15.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
