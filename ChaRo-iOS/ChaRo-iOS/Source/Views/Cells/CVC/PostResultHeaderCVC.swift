0fc8fe684eebbb6df16094499badce751beecc1f
hryeong66 <jhr110326@gmail.com>
2021-07-15 02:30:15 +0900

    ➕[ADD] : #78 - collection view header 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/CVC/PostResultHeaderCVC.swift
@@ -0,0 +1,17 @@
+//
+//  PostResultHeaderCVC.swift
+//  ChaRo-iOS
+//
+//  Created by 장혜령 on 2021/07/15.
+//
+
+import UIKit
+
+class PostResultHeaderCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
