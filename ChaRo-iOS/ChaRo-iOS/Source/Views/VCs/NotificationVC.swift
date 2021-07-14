3b97060560240c2d90d9c351cfd7f46da4b23b09
parkikbum <pibum@naver.com>
2021-07-15 07:37:14 +0900

    ✨[FEAT]: #92 알람 있는 불타는 머리카락  (줄여서 알라모파이어)

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/NotificationVC.swift
@@ -0,0 +1,28 @@
+//
+//  NotificationVC.swift
+//  ChaRo-iOS
+//
+//  Created by 박익범 on 2021/07/15.
+//
+
+import UIKit
+
+class NotificationVC: UIViewController {
+
+    @IBOutlet weak var headerView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    func setShadow(){
+        headerView.getShadowView(color: UIColor.black.cgColor, masksToBounds: false, shadowOffset: CGSize(width: 0, height: 0), shadowRadius: 8, shadowOpacity: 0.3)
+    }
+    
+    @IBAction func backButtonClicked(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+
+}
