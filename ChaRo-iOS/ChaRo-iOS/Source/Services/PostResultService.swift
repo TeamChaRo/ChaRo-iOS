576eca33dd45bba2564f57e3ded8f99e9fc1c213
hryeong66 <jhr110326@gmail.com>
2021-07-15 08:53:59 +0900

    ✨[FEAT] : #84 게시물 상세보기 서버통신 추가

--- ChaRo-iOS/ChaRo-iOS/Source/Services/PostResultService.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Services/PostResultService.swift
@@ -48,11 +48,50 @@ struct PostResultService {
         }
     }
     
+    func getPostDetail(postId : Int,
+                        completion: @escaping (NetworkResult<Any>) -> Void){
+       
+        
+        
+        let dataRequeat = AF.download(Constants.detailPostURL+"\(postId)",
+                                      method: .get,
+                                      encoding: JSONEncoding.default,
+                                      headers: header)
+        
+        dataRequeat.responseData{ dataResponse in
+            switch dataResponse.result{
+            case .success:
+                guard let statusCode = dataResponse.response?.statusCode else { return}
+                guard let value = dataResponse.value  else {return}
+                let networkResult = self.judgeStatusPostDetail(by: statusCode, value)
+                completion(networkResult)
+                
+            case .failure(_): completion(.pathErr)
+            }
+        }
+        
+    }
     
     private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
         let decoder = JSONDecoder()
         
         guard let decodeData = try? decoder.decode(DetailModel.self, from: data) else {
+            return .pathErr
+        }
+        
+        switch statusCode {
+        case 200: return .success(decodeData)
+        case 400: return .requestErr(decodeData)
+        case 500: return .serverErr
+        default:
+            return .networkFail
+        }
+    }
+    
+    private func judgeStatusPostDetail(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        
+        guard let decodeData = try? decoder.decode(PostDatailDataModel.self, from: data) else {
             return .pathErr
         }
         
