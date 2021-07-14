319e78452240b9c109c40f0b82bc6d3d215f4223
indoni <choinj97@gmail.com>
2021-07-15 06:35:54 +0900

    ➕[ADD]: #90 - 작성하기뷰 마지막 셀 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/CreatePostLastTVC.swift
@@ -0,0 +1,24 @@
+//
+//  CreatePostLastTVC.swift
+//  ChaRo-iOS
+//
+//  Created by 최인정 on 2021/07/15.
+//
+
+import UIKit
+
+class CreatePostLastTVC: UITableViewCell {
+
+    static let identifier: String = "CreatePostLastTVC"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        selectionStyle = .none
+    }
+    
+}
