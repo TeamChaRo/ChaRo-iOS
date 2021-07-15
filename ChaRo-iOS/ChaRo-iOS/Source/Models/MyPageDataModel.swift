0f2b97a9fa17150afc178588c3e473cebdc0d796
parkikbum <pibum@naver.com>
2021-07-15 20:21:41 +0900

    🔨[FIX]: #94 - 마이페이지 서버연동 완료

--- ChaRo-iOS/ChaRo-iOS/Source/Models/MyPageDataModel.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Models/MyPageDataModel.swift
@@ -1,7 +1,48 @@
-// This file was generated from JSON Schema using quicktype, do not modify it directly.
-// To parse the JSON, add this file to your project and do:
+//// This file was generated from JSON Schema using quicktype, do not modify it directly.
+//// To parse the JSON, add this file to your project and do:
+////
+////   let myPageDataModel = try? newJSONDecoder().decode(MyPageDataModel.self, from: jsonData)
 //
-//   let myPageDataModel = try? newJSONDecoder().decode(MyPageDataModel.self, from: jsonData)
+//import Foundation
+//
+//// MARK: - MyPageDataModel
+//struct MyPageDataModel: Codable {
+//    let success: Bool
+//    let msg: String
+//    let data: MyPageClass
+//}
+//
+//// MARK: - DataClass
+//struct MyPageClass: Codable {
+//    let userInformation: UserInformation
+//    let writtnTotal: Int
+//    let writtenPost: [Post]
+//    let savedTotal: Int
+//    let savedPost: [Post]
+//}
+//
+//// MARK: - Post
+//struct Post: Codable {
+//    let postID: Int
+//    let title: String
+//    let image: String
+//    let tags: [String]
+//    let favoriteNum, saveNum: Int
+//    let year, month, day: String
+//
+//    enum CodingKeys: String, CodingKey {
+//        case postID = "postId"
+//        case title, image, tags, favoriteNum, saveNum, year, month, day
+//    }
+//}
+//
+//// MARK: - UserInformation
+//struct UserInformation: Codable {
+//    let nickname: String
+//    let profileImage: String
+//    let following: Int
+//    let follower: Int
+//}
 
 import Foundation
 
@@ -15,13 +56,20 @@ struct MyPageDataModel: Codable {
 // MARK: - DataClass
 struct MyPageClass: Codable {
     let userInformation: UserInformation
-    let writtnTotal: Int
+    let writtenTotal: Int
     let writtenPost: [Post]
     let savedTotal: Int
     let savedPost: [Post]
 }
 
-// MARK: - Post
+// MARK: - UserInformation
+struct UserInformation: Codable {
+    let nickname: String
+    let profileImage: String
+    let following, follower: Int
+}
+
+// MARK: - WrittenPost
 struct Post: Codable {
     let postID: Int
     let title: String
@@ -34,11 +82,4 @@ struct Post: Codable {
         case postID = "postId"
         case title, image, tags, favoriteNum, saveNum, year, month, day
     }
-}
-
-// MARK: - UserInformation
-struct UserInformation: Codable {
-    let nickname: String
-    let profileImage: String
-    let following, follower: [String]
 }
