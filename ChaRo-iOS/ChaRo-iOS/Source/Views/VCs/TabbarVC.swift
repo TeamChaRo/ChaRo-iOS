88e9f481174f881bfd8aed9f545172ade2fc9b9b
hryeong66 <jhr110326@gmail.com>
2021-07-16 19:56:14 +0900

    ✅[CHORE] : #106 - 작성하기 데이터랑 연결

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/TabbarVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/TabbarVC.swift
@@ -78,9 +78,6 @@ class TabbarVC: UITabBarController {
         homeTab.tabBarItem = UITabBarItem(title: "구경하기", image: UIImage(named: "tabbarIcHomeInactive"), selectedImage: UIImage(named: "tabbarIcHomeActive"))
         
        
-        
-        
-
         let createStoryboard = UIStoryboard(name: "CreatePost", bundle: nil)
 
         let createVC = createStoryboard.instantiateViewController(identifier: CreatePostVC.identifier)
