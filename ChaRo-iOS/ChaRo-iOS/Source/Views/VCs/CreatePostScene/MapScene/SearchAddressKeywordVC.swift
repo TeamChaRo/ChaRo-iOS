b276198860a23225161037491d8b316b12e33d31
hryeong66 <jhr110326@gmail.com>
2021-09-03 22:24:12 +0900

    ➕[ADD]: #140 - 주소검색 refactor 할 새 파일 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/CreatePostScene/MapScene/SearchAddressKeywordVC.swift
@@ -0,0 +1,29 @@
+//
+//  SearchAddressKeywordVC.swift
+//  ChaRo-iOS
+//
+//  Created by 장혜령 on 2021/09/03.
+//
+
+import UIKit
+
+class SearchAddressKeywordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
