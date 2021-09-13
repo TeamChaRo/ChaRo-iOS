e027834225ad49a1fc64e18917993e044837aee0
Jen <wonee0321@gmail.com>
2021-09-13 20:36:48 +0900

    💄[MOD] : #139 SNS로그인 UI구현

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SignScene/SNSLoginVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SignScene/SNSLoginVC.swift
@@ -22,13 +22,86 @@ class SNSLoginVC: UIViewController {
         $0.image = UIImage(named: "logo")
     }
     
+    let logoLabel = UILabel().then {
+        $0.text = "당신의 드라이브 메이트"
+        $0.font = UIFont.notoSansBoldFont(ofSize: 14)
+        $0.textColor = .mainBlue
+    }
+    
+    let appleLoginBtn = UIButton().then {
+        $0.tintColor = .mainBlack
+    }
+    
+    let googleLoginBtn = UIButton().then {
+        $0.tintColor = .white
+    }
+    
+    let kakaoLoginBtn = UIButton().then {
+        $0.tintColor = .blue
+    }
+    
+    let emailLoginBtn = UIButton().then {
+        $0.setTitle("이메일 로그인", for: .normal)
+        $0.setTitleColor(.gray30, for: .normal)
+        $0.titleLabel?.font = UIFont.notoSansMediumFont(ofSize: 14)
+    }
+    
+    let emailJoinBtn  = UIButton().then {
+        $0.setTitle("이메일로 가입", for: .normal)
+        $0.setTitleColor(.gray30, for: .normal)
+        $0.titleLabel?.font = UIFont.notoSansMediumFont(ofSize: 14)
+    }
+    
     
     func configureUI() {
-        view.addSubview(logoImageView)
+        
+        view.addSubviews([logoImageView,
+                          logoLabel,
+                          appleLoginBtn,
+                          googleLoginBtn,
+                          kakaoLoginBtn,
+                          emailLoginBtn,
+                          emailJoinBtn])
         
         logoImageView.snp.makeConstraints {
-            $0.top.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalToSuperview().offset(273)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(221)
+            $0.height.equalTo(83)
         }
+        
+        logoLabel.snp.makeConstraints {
+            $0.bottom.equalTo(logoImageView.snp.top)
+            $0.height.equalTo(20)
+            $0.centerX.equalToSuperview()
+        }
+        
+//        appleLoginBtn.snp.makeConstraints {
+//
+//        }
+//
+//        googleLoginBtn.snp.makeConstraints {
+//
+//        }
+//
+//        kakaoLoginBtn.snp.makeConstraints {
+//
+//        }
+//
+        emailLoginBtn.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-55)
+            $0.width.equalTo(81)
+            $0.height.equalTo(21)
+            $0.trailing.equalTo(view.snp.centerX).offset(-12.5)
+        }
+        
+        emailJoinBtn.snp.makeConstraints {
+            $0.bottom.equalToSuperview().offset(-55)
+            $0.width.equalTo(81)
+            $0.height.equalTo(21)
+            $0.leading.equalTo(view.snp.centerX).offset(12.5)
+        }
+        
     }
     
     
