34b8426b29f9bfbee7137637ada4afc056663802
Jen <wonee0321@gmail.com>
2021-09-16 19:53:50 +0900

    ➕[ADD] : #139 JoinViewCVC 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/CVC/JoinScene/JoinViewCVC.swift
@@ -0,0 +1,14 @@
+//
+//  JoinViewCVC.swift
+//  ChaRo-iOS
+//
+//  Created by JEN Lee on 2021/09/16.
+//
+
+import UIKit
+
+class JoinViewCVC: UICollectionViewCell {
+    
+    static let identifier = "JoinViewCVC"
+    
+}
