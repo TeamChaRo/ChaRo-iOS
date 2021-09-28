57bb5ab1c2d1f404b924e26436f7bbd405403a42
Jen <wonee0321@gmail.com>
2021-09-28 16:19:26 +0900

    ➕[ADD] : #139 회원가입 창 input 템플릿 뷰 생성

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/JoinInputView.swift
@@ -0,0 +1,20 @@
+//
+//  JoinInputView.swift
+//  ChaRo-iOS
+//
+//  Created by JEN Lee on 2021/09/28.
+//
+
+import UIKit
+
+class JoinInputView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
