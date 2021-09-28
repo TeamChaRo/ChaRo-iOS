5472589284df38c89116358561b88be7bf182f36
Jen <wonee0321@gmail.com>
2021-09-28 16:44:35 +0900

    ✨[FEAT] : #139 공통 뷰 컴포넌트 구현 완료

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/JoinInputView.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/JoinInputView.swift
@@ -8,13 +8,66 @@
 import UIKit
 
 class JoinInputView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    var titleLabel = UILabel().then {
+        $0.font = .notoSansBoldFont(ofSize: 17)
+        $0.textColor = .mainBlack
     }
-    */
+
+    var subTitleLabel = UILabel().then {
+        $0.font = .notoSansRegularFont(ofSize: 11)
+        $0.textColor = .gray40
+    }
+
+    var inputTextField = UITextField().then {
+        $0.backgroundColor = .gray10
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 10
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.gray20.cgColor
+        $0.addLeftPadding(14)
+        $0.clearButtonMode = .whileEditing
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(title: String, subTitle: String, placeholder: String) {
+        super.init(frame: .zero)
+        
+        titleLabel.text = title
+        subTitleLabel.text = subTitle
+        inputTextField.placeholder = placeholder
+        
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        addSubviews([titleLabel,
+                     subTitleLabel,
+                     inputTextField])
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.snp.top)
+            $0.leading.equalTo(self.snp.leading)
+            $0.trailing.equalTo(self.snp.trailing)
+        }
+        
+        subTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(2)
+            $0.leading.trailing.equalTo(titleLabel)
+        }
+        
+        inputTextField.snp.makeConstraints {
+            $0.top.equalTo(subTitleLabel.snp.bottom).offset(12)
+            $0.leading.trailing.equalTo(titleLabel)
+            $0.height.equalTo(48)
+        }
+    }
 
 }
