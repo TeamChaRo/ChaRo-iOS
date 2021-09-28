1c487e462718a4a4891c22610f97b2dc9d91431a
Jen <wonee0321@gmail.com>
2021-09-28 19:41:13 +0900

    ✨[FEAT] : #139 InputTextField 눈 버튼에 따라 암호 보이고 안보이고 기능 구현

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/InputTextField.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/InputTextField.swift
@@ -14,7 +14,7 @@ enum InputTextFieldType {
 
 class InputTextField: UITextField {
     
-    var iconClick = false
+    var iconClick = true
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -26,8 +26,8 @@ class InputTextField: UITextField {
     }
     
     init(type: InputTextFieldType, placeholder: String) {
-        
         super.init(frame: .zero)
+        
         self.placeholder = placeholder
         self.backgroundColor = .gray10
         self.clipsToBounds = true
@@ -46,18 +46,62 @@ class InputTextField: UITextField {
             self.clearButtonMode = .whileEditing
         case .password:
             self.isSecureTextEntry = true
+            configureShowPasswordButton()
         }
     }
     
     private func configureShowPasswordButton() {
         
+        let iconImageView = UIImageView()
+        let eyeOnImage = UIImage(named: "icEyeOn")
+        let eyeOffImage = UIImage(named: "icEyeOff")
+        
+        iconImageView.image = eyeOffImage
+        
+        let contentView = UIView()
+        contentView.addSubview(iconImageView)
+        
+        contentView.frame = CGRect(x: 0, y: 0,
+                                   width: eyeOnImage!.size.width,
+                                   height: eyeOnImage!.size.height)
+        
+        iconImageView.frame = CGRect(x: -10, y: 0,
+                                     width: eyeOnImage!.size.width,
+                                     height: eyeOnImage!.size.height)
+        
+        self.rightView = contentView
+        self.rightViewMode = .always
+        
+        
+        //tapGestureRecognizer 생성
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        iconImageView.isUserInteractionEnabled = true
+        iconImageView.addGestureRecognizer(tapGestureRecognizer)
+        
     }
     
-    public func setBlueBorderWithText(type: InputTextFieldType, text: String) {
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        let tappedImage = tapGestureRecognizer.view as! UIImageView
+        
+        //iconClick 변수의 값에 따라 비밀번호 보이기 안보이기
+        if iconClick {
+            iconClick = false
+            tappedImage.image = UIImage(named: "icEyeOn")
+            self.isSecureTextEntry = false
+        }
+        else {
+            iconClick = true
+            tappedImage.image = UIImage(named: "icEyeOff")
+            self.isSecureTextEntry = true
+        }
+    }
+    
+    //사용자에게 텍스트와 함께 테두리 색상을 변경. 텍스트의 위치 조정을 위해 type 과 isFirst 인자를 받음, type이 normal일 때는 isFirst에 nil
+    public func setBlueBorderWithText(text: String, type: InputTextFieldType, isFirst: Bool?) {
         self.layer.borderColor = UIColor.mainBlue.cgColor
     }
     
-    public func setRedBorderWithText(type: InputTextFieldType, text: String) {
+    public func setRedBorderWithText(text: String, type: InputTextFieldType, isFirst: Bool?) {
         self.layer.borderColor = UIColor.mainOrange.cgColor
     }
     
