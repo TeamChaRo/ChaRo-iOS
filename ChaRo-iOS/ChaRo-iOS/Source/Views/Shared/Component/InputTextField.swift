e98226fb37a86f7f0040f0937729c3328e8bd22d
Jen <wonee0321@gmail.com>
2021-09-28 18:42:23 +0900

    ➕[ADD] : #139 InputTextField 기본 틀 정의

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/InputTextField.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/InputTextField.swift
@@ -7,7 +7,36 @@
 
 import UIKit
 
+enum InputTextFieldType {
+    case normal
+    case password
+}
+
 class InputTextField: UITextField {
-
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+       required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(type: InputTextFieldType, placeholder: String) {
+        super.init(frame: .zero)
+        self.placeholder = placeholder
+        self.backgroundColor = .gray10
+        self.clipsToBounds = true
+        self.layer.cornerRadius = 10
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor.gray20.cgColor
+        self.addLeftPadding(14)
+        
+        self.attributedPlaceholder = NSAttributedString(string: placeholder,
+                                                        attributes: [
+                                                            .foregroundColor: UIColor.lightGray,
+                                                            .font: UIFont.notoSansRegularFont(ofSize: 14)
+                                                        ])
+    }
 }
