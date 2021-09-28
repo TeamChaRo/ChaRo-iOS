1d73bef5b96348520352f5fa956de26b9ec64437
Jen <wonee0321@gmail.com>
2021-09-28 18:56:21 +0900

    ➕[ADD] : #139 InputTextField 함수 생성

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/InputTextField.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/InputTextField.swift
@@ -14,6 +14,8 @@ enum InputTextFieldType {
 
 class InputTextField: UITextField {
     
+    var iconClick = false
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -24,6 +26,7 @@ class InputTextField: UITextField {
     }
     
     init(type: InputTextFieldType, placeholder: String) {
+        
         super.init(frame: .zero)
         self.placeholder = placeholder
         self.backgroundColor = .gray10
@@ -32,11 +35,31 @@ class InputTextField: UITextField {
         self.layer.borderWidth = 1
         self.layer.borderColor = UIColor.gray20.cgColor
         self.addLeftPadding(14)
-        
         self.attributedPlaceholder = NSAttributedString(string: placeholder,
                                                         attributes: [
                                                             .foregroundColor: UIColor.lightGray,
                                                             .font: UIFont.notoSansRegularFont(ofSize: 14)
                                                         ])
+        
+        switch type {
+        case .normal:
+            self.clearButtonMode = .whileEditing
+        case .password:
+            self.isSecureTextEntry = true
+        }
     }
+    
+    private func configureShowPasswordButton() {
+        
+    }
+    
+    public func setBlueBorderWithText(type: InputTextFieldType, text: String) {
+        self.layer.borderColor = UIColor.mainBlue.cgColor
+    }
+    
+    public func setRedBorderWithText(type: InputTextFieldType, text: String) {
+        self.layer.borderColor = UIColor.mainOrange.cgColor
+    }
+    
+    
 }
