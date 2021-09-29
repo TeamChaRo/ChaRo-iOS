7360fdc4e604e2a0973dad2530db14a7abf777ae
Jen <wonee0321@gmail.com>
2021-09-30 00:29:15 +0900

    💄[MOD] : #139 ProfileView UI 설정

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/ProfileView.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/ProfileView.swift
@@ -19,10 +19,28 @@ class ProfileView: UIView, UIImagePickerControllerDelegate {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        setConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setConstraints() {
+        addSubviews([profileImageView,
+                     cameraButton])
+        
+        profileImageView.snp.makeConstraints {
+            $0.top.equalTo(self.snp.top)
+            $0.leading.equalTo(self.snp.leading)
+            $0.width.height.equalTo(112)
+        }
+        
+        cameraButton.snp.makeConstraints {
+            $0.top.equalTo(self.snp.top).offset(72)
+            $0.leading.equalTo(self.snp.leading).offset(72)
+            $0.width.height.equalTo(48)
+        }
     }
     
     @objc func carmeraButtonClicked() {
