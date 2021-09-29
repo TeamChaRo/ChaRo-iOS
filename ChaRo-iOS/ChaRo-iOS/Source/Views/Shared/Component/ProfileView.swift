7aa5be31ba2a45f9f7d0e51fbc2082795ad9aba9
Jen <wonee0321@gmail.com>
2021-09-30 00:24:49 +0900

    ➕[ADD] : #139 ProfileView 함수 셋팅

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/ProfileView.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/ProfileView.swift
@@ -7,14 +7,26 @@
 
 import UIKit
 
-class ProfileView: UIView {
+class ProfileView: UIView, UIImagePickerControllerDelegate {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    var profileImageView = UIImageView().then {
+        $0.image = UIImage(named: "")
     }
-    */
+    
+    var cameraButton = UIButton().then {
+        $0.addTarget(self, action: #selector(carmeraButtonClicked), for: .touchUpInside)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc func carmeraButtonClicked() {
+        
+    }
 
 }
