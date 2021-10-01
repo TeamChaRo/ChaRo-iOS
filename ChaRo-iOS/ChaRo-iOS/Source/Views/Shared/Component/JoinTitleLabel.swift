642943fb2fb23dcf428ea02bf78bb330525e84e8
Jen <wonee0321@gmail.com>
2021-10-01 17:16:33 +0900

    ➕[ADD] : #139 JoinTitleLabelType enum 생성

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/JoinTitleLabel.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/JoinTitleLabel.swift
@@ -5,10 +5,26 @@
 //  Created by JEN Lee on 2021/10/01.
 //
 
+enum JoinTitleLabelType {
+    case boldTitle
+    case normalTitle
+}
+
 import UIKit
 
 class JoinTitleLabel: UILabel {
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+   
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init() {
+        super.init(frame: .zero)
+    }
     
 
 }
