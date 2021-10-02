328056ba92f9fab4fa9cc9576767b792bd7ecc79
parkikbum <pibum@naver.com>
2021-10-03 01:29:41 +0900

    ✅[CHORE]:#149 - 서버연동 url및 데이터 모델 수정

--- ChaRo-iOS/ChaRo-iOS/Source/Models/MyPageDataModel.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Models/MyPageDataModel.swift
@@ -1,8 +1,3 @@
-//// This file was generated from JSON Schema using quicktype, do not modify it directly.
-//// To parse the JSON, add this file to your project and do:
-////
-////   let myPageDataModel = try? newJSONDecoder().decode(MyPageDataModel.self, from: jsonData)
-//
 //import Foundation
 //
 //// MARK: - MyPageDataModel
@@ -15,13 +10,20 @@
 //// MARK: - DataClass
 //struct MyPageClass: Codable {
 //    let userInformation: UserInformation
-//    let writtnTotal: Int
+//    let writtenTotal: Int
 //    let writtenPost: [Post]
 //    let savedTotal: Int
 //    let savedPost: [Post]
 //}
 //
-//// MARK: - Post
+//// MARK: - UserInformation
+//struct UserInformation: Codable {
+//    let nickname: String
+//    let profileImage: String
+//    let following, follower: Int
+//}
+//
+//// MARK: - WrittenPost
 //struct Post: Codable {
 //    let postID: Int
 //    let title: String
@@ -35,14 +37,7 @@
 //        case title, image, tags, favoriteNum, saveNum, year, month, day
 //    }
 //}
-//
-//// MARK: - UserInformation
-//struct UserInformation: Codable {
-//    let nickname: String
-//    let profileImage: String
-//    let following: Int
-//    let follower: Int
-//}
+
 
 import Foundation
 
@@ -50,16 +45,41 @@ import Foundation
 struct MyPageDataModel: Codable {
     let success: Bool
     let msg: String
-    let data: MyPageClass
+    let data: MyPageDataClass
 }
 
 // MARK: - DataClass
-struct MyPageClass: Codable {
+struct MyPageDataClass: Codable {
     let userInformation: UserInformation
-    let writtenTotal: Int
-    let writtenPost: [Post]
-    let savedTotal: Int
-    let savedPost: [Post]
+    let writtenPost, savedPost: MyPagePost
+}
+
+// MARK: - Post
+struct MyPagePost: Codable {
+    let lastID, lastCount: Int
+    let drive: [MyPageDrive]
+
+    enum CodingKeys: String, CodingKey {
+        case lastID = "lastId"
+        case lastCount, drive
+    }
+}
+
+// MARK: - Drive
+struct MyPageDrive: Codable {
+    let postID: Int
+    let title: String
+    let image: String
+    let region, theme: String
+    let warning: String?
+    let year, month, day: String
+    let isFavorite: Bool
+    let favoriteNum, saveNum: Int
+
+    enum CodingKeys: String, CodingKey {
+        case postID = "postId"
+        case title, image, region, theme, warning, year, month, day, isFavorite, favoriteNum, saveNum
+    }
 }
 
 // MARK: - UserInformation
@@ -67,19 +87,4 @@ struct UserInformation: Codable {
     let nickname: String
     let profileImage: String
     let following, follower: Int
-}
-
-// MARK: - WrittenPost
-struct Post: Codable {
-    let postID: Int
-    let title: String
-    let image: String
-    let tags: [String]
-    let favoriteNum, saveNum: Int
-    let year, month, day: String
-
-    enum CodingKeys: String, CodingKey {
-        case postID = "postId"
-        case title, image, tags, favoriteNum, saveNum, year, month, day
-    }
 }
