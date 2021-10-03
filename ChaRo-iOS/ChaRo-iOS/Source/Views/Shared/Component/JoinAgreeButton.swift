231b58a6b8b1ae3c8281dce0ac208e5699eb340d
Jen <wonee0321@gmail.com>
2021-10-04 03:58:20 +0900

    ➕[ADD] : #139 JoinAgreeButton 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/JoinAgreeButton.swift
@@ -0,0 +1,20 @@
+//
+//  JoinAgreeButton.swift
+//  ChaRo-iOS
+//
+//  Created by JEN Lee on 2021/10/04.
+//
+
+import UIKit
+
+class JoinAgreeButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
