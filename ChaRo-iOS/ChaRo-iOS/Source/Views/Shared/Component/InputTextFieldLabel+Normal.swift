a118441f9b413b496ea3307d46eed78d64503eb5
Jen <wonee0321@gmail.com>
2021-10-05 00:07:18 +0900

    ➕[ADD] : #139 텍스트필드 공통 뷰 생성

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/InputTextFieldLabel+Normal.swift
@@ -0,0 +1,14 @@
+//
+//  InputTextFieldLabel+Normal.swift
+//  ChaRo-iOS
+//
+//  Created by JEN Lee on 2021/10/05.
+//
+
+import UIKit
+
+class InputTextFieldLabel_Normal: UIView {
+
+    
+
+}
---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/InputTextFieldLabel_Password.swift
@@ -0,0 +1,14 @@
+//
+//  InputTextFieldLabel_Password.swift
+//  ChaRo-iOS
+//
+//  Created by JEN Lee on 2021/10/05.
+//
+
+import UIKit
+
+class InputTextFieldLabel_Password: UIView {
+
+    
+
+}
