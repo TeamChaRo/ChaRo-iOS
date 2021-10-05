92a9bae1eee8cb75177ce92e8190b0a08c18c465
Jen <wonee0321@gmail.com>
2021-10-06 03:21:46 +0900

    ✨[FEAT] : #139 nextButton 클로저 생성

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/NextButton.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Shared/Component/NextButton.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class NextButton: UIButton {
 
+    var nextPageClosure: (() -> Void)?
+    var nextViewClosure: (() -> Void)?
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
@@ -20,6 +23,19 @@ class NextButton: UIButton {
     init(isSticky: Bool) {
         super.init(frame: .zero)
         configureUI(isSticky: isSticky)
+        self.addTarget(self, action: #selector(nextButtonClicked(isTheLast:)), for: .touchUpInside)
+    }
+    
+    //다음 버튼이 마지막 단계일 경우 다음 페이지 뷰로 이동 -> JoinVC에서 클로저 구현
+    //다음 단계가 있을 경우 -> 각 View에서 클로저 구현
+    @objc func nextButtonClicked(isTheLast: Bool) {
+        
+        if isTheLast {
+            self.nextPageClosure!()
+        } else {
+            self.nextViewClosure!()
+        }
+        
     }
     
     private func configureUI(isSticky: Bool) {
