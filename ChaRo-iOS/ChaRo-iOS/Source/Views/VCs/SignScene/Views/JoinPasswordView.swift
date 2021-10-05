834f5ea06248454fb36001472e677a3be0e609a1
Jen <wonee0321@gmail.com>
2021-10-05 17:23:08 +0900

    ✅[CHORE] : #139 JoinPasswordViewUI 구현

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SignScene/Views/JoinPasswordView.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SignScene/Views/JoinPasswordView.swift
@@ -9,12 +9,44 @@ import UIKit
 
 class JoinPasswordView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let passwordInputView = PasswordView(title: "비밀번호", subTitle: "5자 이상 15자 이내의 비밀번호를 입력해주세요.")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    init() {
+        super.init(frame: .zero)
+        configureUI()
+    }
+    
+    private func configureUI() {
+                
+        self.addSubviews([passwordInputView])
+        
+        passwordInputView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(20)
+            $0.trailing.equalToSuperview().offset(-20)
+            $0.height.equalTo(200)
+        }
+        
+        passwordInputView.enableNextButtonClosure = {
+            //self.nextButton.setTitle("됐다!!!", for: .normal)
+            print("나중에 자기 버튼 만들어서 넣기")
+        }
+        passwordInputView.unableNextButtonClosure = {
+            //self.nextButton.setTitle("다음", for: .normal)
+            print("나중에 자기 버튼 만들어서 넣기")
+        }
+        
+        self.dismissKeyboardWhenTappedAround()
+        
+    }
 
 }
