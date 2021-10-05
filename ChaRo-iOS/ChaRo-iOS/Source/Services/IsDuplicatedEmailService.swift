37ac4e14eb51068cc1d8b22d27dd53bc874aab10
Jen <wonee0321@gmail.com>
2021-10-05 19:15:12 +0900

    ✅[CHORE] : #139 IsDuplicatedEmailService 구현

--- ChaRo-iOS/ChaRo-iOS/Source/Services/IsDuplicatedEmailService.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Services/IsDuplicatedEmailService.swift
@@ -6,8 +6,62 @@
 //
 
 import Foundation
+import Alamofire
 
 
 struct IsDuplicatedEmailService {
-    <#fields#>
+    static let shared = IsDuplicatedEmailService()
+    
+    func getPersonInfo(completion : @escaping (NetworkResult<Any>) -> Void)
+        {
+    
+        let URL = Constants.duplicateEmailURL
+            let header : HTTPHeaders = ["Content-Type": "application/json"]
+            
+            let dataRequest = AF.request(URL,
+                                         method: .get,
+                                         encoding: JSONEncoding.default,
+                                         headers: header)
+            
+            
+            dataRequest.responseData { dataResponse in
+                
+                
+                switch dataResponse.result {
+                case .success:
+                                    
+                    print("----- 데이터 요청 성공")
+                    guard let statusCode = dataResponse.response?.statusCode else {return}
+                    guard let value = dataResponse.value else {return}
+                    let networkResult = self.judgeStatus(by: statusCode, value)
+                    completion(networkResult)
+                
+                case .failure: completion(.pathErr)
+                    
+                }
+            }
+                                                
+        }
+    
+    private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        
+        let decoder = JSONDecoder()
+        
+        guard let decodedData = try? decoder.decode(LikeDataModel.self, from: data)
+        else {
+            print("패쓰에러")
+            return .pathErr
+        }
+        
+        switch statusCode {
+        case 200:
+            print("--- 데이터 받기 성공")
+            return .success(decodedData.success)
+        case 400: return .requestErr(decodedData.msg)
+        case 500: return .serverErr
+        default: return .networkFail
+        }
+        
+    }
+    
 }
