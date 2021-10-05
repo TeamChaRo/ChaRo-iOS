f1f01171c93a1dd9602ebc328aca72f4790fd1c5
Jen <wonee0321@gmail.com>
2021-10-05 17:24:13 +0900

    ➕[ADD] : #139 JoinProfileView 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SignScene/Views/JoinProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  JoinProfileView.swift
+//  ChaRo-iOS
+//
+//  Created by JEN Lee on 2021/10/05.
+//
+
+import UIKit
+
+class JoinProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
