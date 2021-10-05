67f7f26d61a2db9867a86bef8c58ac4da7dcf60a
Jen <wonee0321@gmail.com>
2021-10-05 17:06:46 +0900

    ➕[ADD] : #139 JoinEmailView 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SignScene/Views/JoinEmailView.swift
@@ -0,0 +1,20 @@
+//
+//  JoinEmailView.swift
+//  ChaRo-iOS
+//
+//  Created by JEN Lee on 2021/10/05.
+//
+
+import UIKit
+
+class JoinEmailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
