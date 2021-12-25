b7a4fb2d60b9c5947740563217e52de6e6d22131
parkikbum <pibum@naver.com>
2021-12-26 01:15:18 +0900

    ➕[ADD]:#165-팔로잉팔로우리스트 뷰컨 및 스보 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/FollowScene/FollowFollwingVC.swift
@@ -0,0 +1,20 @@
+//
+//  FollowFollwingVC.swift
+//  ChaRo-iOS
+//
+//  Created by 박익범 on 2021/12/24.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class FollowFollwingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
