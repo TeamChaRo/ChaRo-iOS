fa403f140290af13aab2caf313b1f736458de5c6
hryeong66 <jhr110326@gmail.com>
2021-12-28 23:37:59 +0900

    ✅[CHORE] #170 테스트 항상 성공하도록 연결

--- ChaRo-iOS/ChaRo-iOSTests/ChaRo_iOSTests.swift
+++ ChaRo-iOS/ChaRo-iOSTests/ChaRo_iOSTests.swift
@@ -23,6 +23,7 @@ class ChaRo_iOSTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        XCTAssert(true)
     }
 
     func testPerformanceExample() throws {
