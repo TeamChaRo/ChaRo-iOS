eb99e004ee78f94af78ff3225e39d64500fbc42f
parkikbum <pibum@naver.com>
2021-12-30 23:47:31 +0900

    ➕[ADD]: #174 - 세팅 뷰 데이터 모델 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Models/SettingViewDataModel.swift
@@ -0,0 +1,43 @@
+import Foundation
+import UIKit
+
+struct settingDataModel{
+    var isToggle: Bool = false
+    var toggleData: Bool = false
+    var isSubLabel: Bool = false
+    var subLabelString: String = ""
+    var titleString: String = ""
+    var titleLabelColor: UIColor = UIColor.black
+    var subLabelColor: UIColor = UIColor.white
+    
+    init(){
+        
+    }
+    
+    //토글 있는 부분 init
+    init(titleString: String, isToggle: Bool, toggleData: Bool) {
+        self.titleString = titleString
+        self.isToggle = isToggle
+        self.toggleData = toggleData
+    }
+    //좌측에 문구 있는 부분 init
+    init(isSubLabel: Bool, subLabelString: String, subLabelColor: UIColor) {
+        self.isSubLabel = isSubLabel
+        self.subLabelString = subLabelString
+        self.subLabelColor = subLabelColor
+    }
+    //기본 글씨 색상 변경 부분 init
+    init(titleString: String, titleLabelColor: UIColor){
+        self.titleString = titleString
+        self.titleLabelColor = titleLabelColor
+    }
+    //기본글씨 + 좌측 글씨 + 색상
+    init(titleString: String, titleLabelColor: UIColor, isSubLabel: Bool, subLabelString: String, subLabelColor: UIColor){
+        self.titleString = titleString
+        self.titleLabelColor = titleLabelColor
+        self.isSubLabel = isSubLabel
+        self.subLabelString = subLabelString
+        self.subLabelColor = subLabelColor
+    }
+
+}
---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/SettingScene/SettingTVC.swift
@@ -0,0 +1,23 @@
+//
+//  SettingTVC.swift
+//  ChaRo-iOS
+//
+//  Created by 박익범 on 2021/12/29.
+//
+
+import UIKit
+
+class SettingTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
