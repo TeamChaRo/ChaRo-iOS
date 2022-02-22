ef18add7693385ef944d4af32219b12aeff44c7a
hryeong66 <jhr110326@gmail.com>
2022-02-22 21:20:36 +0900

    ➕[ADD]:#182 - 상세보기 하단뷰에서 사용할 아이콘 5가지 추가

--- ChaRo-iOS/ChaRo-iOS/Resource/Literals/ImageLiterals.swift
+++ ChaRo-iOS/ChaRo-iOS/Resource/Literals/ImageLiterals.swift
@@ -5,4 +5,33 @@
 //  Created by 장혜령 on 2022/02/22.
 //
 
-import Foundation
+import UIKit
+
+enum ImageLiterals {
+    //MARK: - icon
+    static var icHeartActive: UIImage{ .load(name: "icHeartActive") }
+    static var icHeartInactive: UIImage{ .load(name: "icHeartInactive") }
+    static var icScrapActive: UIImage{ .load(name: "icSaveActive") }
+    static var icScrapInactive: UIImage{ .load(name: "icSaveInactive") }
+    static var icShare: UIImage{ .load(name: "icShare") }
+    
+    //MARK: - image
+    
+}
+
+extension UIImage {
+    static func load(name: String) -> UIImage {
+        guard let image = UIImage(named: name, in: nil, compatibleWith: nil) else {
+            return UIImage()
+        }
+        image.accessibilityIdentifier = name
+        return image
+    }
+    
+    func resize(to size: CGSize) -> UIImage {
+        let image = UIGraphicsImageRenderer(size: size).image { _ in
+            draw(in: CGRect(origin: .zero, size: size))
+        }
+        return image
+    }
+}
