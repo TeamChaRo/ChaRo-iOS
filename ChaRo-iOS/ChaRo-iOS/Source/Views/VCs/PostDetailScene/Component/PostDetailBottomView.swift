6a5d7d6edfb272b6cc6a1774c631ced17614af4b
hryeong66 <jhr110326@gmail.com>
2022-02-22 21:14:17 +0900

    ➕[ADD] :#182 - 상세보기 뷰에 들어갈 하단 뷰 클래스 생성

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/PostDetailScene/Component/PostDetailBottomView.swift
@@ -0,0 +1,21 @@
+//
+//  PostDetailBottomView.swift
+//  ChaRo-iOS
+//
+//  Created by 장혜령 on 2022/02/22.
+//
+
+import UIKit
+
+class PostDetailBottomView: UIView{
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
