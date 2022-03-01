c77ce07972d1ef1836ee390fb3875fd325f6c3de
Jen <wonee0321@gmail.com>
2022-03-01 22:30:53 +0900

    [ADD] #153 VC 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SettingScene/ChangePasswordVC.swift
@@ -0,0 +1,29 @@
+//
+//  ChangePasswordVC.swift
+//  ChaRo-iOS
+//
+//  Created by JEN Lee on 2022/03/01.
+//
+
+import UIKit
+
+class ChangePasswordVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
