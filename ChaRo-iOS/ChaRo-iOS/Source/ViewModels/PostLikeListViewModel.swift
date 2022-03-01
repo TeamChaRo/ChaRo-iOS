b3f55e18685e61b212a19a2e58d7e70d612f19d1
hryeong66 <jhr110326@gmail.com>
2022-03-02 03:41:16 +0900

    ➕[ADD] 좋아요 목록 보여줄 VC와 ViewModel 추가 (#155)

---
+++ ChaRo-iOS/ChaRo-iOS/Source/ViewModels/PostLikeListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostLikeListViewModel.swift
+//  ChaRo-iOS
+//
+//  Created by 장혜령 on 2022/03/02.
+//
+
+import Foundation
---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/PostDetailScene/PostLikeListVC.swift
@@ -0,0 +1,8 @@
+//
+//  PostLikeListVC.swift
+//  ChaRo-iOS
+//
+//  Created by 장혜령 on 2022/03/02.
+//
+
+import Foundation
