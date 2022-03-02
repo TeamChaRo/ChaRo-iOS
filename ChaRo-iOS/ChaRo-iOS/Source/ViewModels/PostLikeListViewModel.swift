ba5ac948c7560150a13eb150882b385a64a0410b
hryeong66 <jhr110326@gmail.com>
2022-03-03 02:53:12 +0900

    ✅[CHORE] #155 - 로직 잘못된 부분 수정

--- ChaRo-iOS/ChaRo-iOS/Source/ViewModels/PostLikeListViewModel.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/ViewModels/PostLikeListViewModel.swift
@@ -14,8 +14,9 @@ class PostLikeListViewModel{
     //MARK: - Properties
     let defaultHeight: CGFloat = UIScreen.getDeviceHeight() / 2
     let dismissibleHeight: CGFloat = 200
-    let maximumContainerHeight: CGFloat = UIScreen.getDeviceHeight() - 200
+    let maximumContainerHeight: CGFloat = UIScreen.getDeviceHeight() - 64
     var currentContainerHeight: CGFloat = UIScreen.getDeviceHeight() / 2
+    let maxBackgroundAlpha: CGFloat = 0.8
     
     //MARK: - Input and Output
     struct Input{
@@ -28,13 +29,12 @@ class PostLikeListViewModel{
     
     func transform(form input: Input) -> Output{
         let output = Output()
-        
         input.transionYOffsetSubject
             .bind(onNext: { [weak self] (yOffset, isDragging) in
                 guard let self = self else {return}
-                print("----------isDragging = \(isDragging)")
-                output.newHeightSubject.onNext((self.calculateHeight(yOffset: yOffset, isDragging: !isDragging),
-                                                isDragging))
+                output.newHeightSubject
+                    .onNext((self.calculateHeight(yOffset: yOffset, isDragging: isDragging),
+                                                !isDragging))
             })
             .disposed(by: self.disposeBag)
         return output
@@ -43,9 +43,7 @@ class PostLikeListViewModel{
     private func calculateHeight(yOffset: CGFloat, isDragging: Bool) -> CGFloat{
         let isDraggingDown = yOffset > 0
         let newHeight = self.currentContainerHeight - yOffset
-        
-        print("newHeight = \(newHeight), 현재 드래그 \(isDraggingDown ? "내려가는 중" : "올라가는 중")")
-        if isDragging{
+        if isDragging {
             return newHeight
         }
         
@@ -56,10 +54,8 @@ class PostLikeListViewModel{
         }else if newHeight < self.maximumContainerHeight && isDraggingDown{
             return self.defaultHeight
         }else if newHeight > self.defaultHeight && !isDraggingDown{
-            print("여기에 걸여야 하는거 아니야?")
             return self.maximumContainerHeight
         }
-        print("여기중에 안걸리는 경우가 있나?")
         return -1
     }
 }
