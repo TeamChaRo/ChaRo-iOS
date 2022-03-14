dcd3d4159cd8ee151d0e0792e6994e83c8101249
hryeong66 <jhr110326@gmail.com>
2022-03-14 23:30:44 +0900

    ✅[CHORE] #196 - postTitleTVC xib 삭제 후 코드로 ui 구성

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/PostDetailScene/PostTitleTVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/PostDetailScene/PostTitleTVC.swift
@@ -6,53 +6,82 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class PostTitleTVC: UITableViewCell {
-
-    static let identifier: String = "PostTitleTVC"
     
-    // MARK: - Outlet Variables
-
-    @IBOutlet weak var postTitleLabel: UILabel!
-    @IBOutlet weak var profileImageView: UIImageView!
-    @IBOutlet weak var userNameLabel: UILabel!
-    @IBOutlet weak var postDateLabel: UILabel!
-    @IBOutlet weak var likedImageView: UIImageView!
-    @IBOutlet weak var likedCountLabel: UILabel!
+    // MARK: - properties
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        selectionStyle = .none
-        setFont()
+    private let postTitleLabel = UILabel().then {
+        $0.font = .notoSansBoldFont(ofSize: 19)
+        $0.textColor = .mainBlack
     }
-
+    
+    private let userNameLabel = UILabel().then {
+        $0.font = .notoSansMediumFont(ofSize: 13)
+        $0.textColor = .mainBlack
+    }
+    
+    private let postDateLabel = UILabel().then {
+        $0.font = .notoSansRegularFont(ofSize: 12)
+        $0.textColor = .gray30
+    }
+    
+    private let profileImageView = UIImageView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupConstraints()
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
     }
     
-    func setFont(){
-        postTitleLabel.font = UIFont.systemFont(ofSize: 19, weight: .bold)
-        userNameLabel.font = UIFont.systemFont(ofSize: 13, weight: .medium)
-        postDateLabel.font = UIFont.systemFont(ofSize: 12)
+    func setupConstraints() {
+        addSubviews([postTitleLabel, profileImageView,
+                     userNameLabel, postDateLabel])
+        
+        postTitleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(21)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        profileImageView.snp.makeConstraints {
+            $0.top.equalTo(postTitleLabel.snp.bottom).inset(15)
+            $0.leading.equalTo(postTitleLabel.snp.leading)
+            $0.width.height.equalTo(29)
+            $0.bottom.equalToSuperview().inset(17)
+        }
+        
+        userNameLabel.snp.makeConstraints {
+            $0.top.equalTo(postTitleLabel.snp.bottom).inset(10)
+            $0.leading.equalTo(profileImageView.snp.trailing).inset(14)
+        }
+        
+        postDateLabel.snp.makeConstraints{
+            $0.top.equalTo(userNameLabel.snp.bottom).inset(1)
+            $0.leading.equalTo(userNameLabel.snp.leading)
+        }
     }
     
-    func setTitle(title: String, userName: String, date: String, imageName: String, likedCount: String) {
+    func configureUI() {
+        selectionStyle = .none
+    }
+    
+    func setContent(title: String, userName: String, date: String, imageName: String) {
         postTitleLabel.text = title
         userNameLabel.text = userName
         postDateLabel.text = date
-        likedCountLabel.text = likedCount
-    
         profileImageView.layer.masksToBounds = true
         profileImageView.kf.setImage(with: URL(string: imageName))
         profileImageView.layer.cornerRadius = profileImageView.frame.height / 2.0
-        
-//        if let image = UIImage(named: imageName) {
-//            profileImageView.layer.masksToBounds = true
-//
-//            profileImageView.image = image
-//            profileImageView.layer.cornerRadius = profileImageView.frame.height / 2.0
-//        }
     }
     
 }
