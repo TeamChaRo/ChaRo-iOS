777c7d4c4ea4d19c167dc844ec9d83f741b11761
hryeong66 <jhr110326@gmail.com>
2022-03-15 22:21:41 +0900

    ✨[FEAT] #196 - toast 메세지 보여주는 함수 추가

--- ChaRo-iOS/ChaRo-iOS/Source/Extensions/UIView+.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Extensions/UIView+.swift
@@ -44,5 +44,24 @@ extension UIView {
     @objc func dismissKeyboard() {
         self.endEditing(true)
     }
+    
+    func showToast(message : String) {
+        let toastLabel = UILabel(frame: CGRect(x: UIScreen.getDeviceWidth() - 100,
+                                               y: UIScreen.getDeviceHeight() - 150,
+                                               width: 200, height: 35))
+        toastLabel.backgroundColor = .gray40
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = .notoSansRegularFont(ofSize: 14)
+        toastLabel.textAlignment = .center
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 17;
+        toastLabel.clipsToBounds = true
+        self.addSubview(toastLabel)
+        UIView.animate(withDuration: 2.0,
+                       delay: 0.5, options: .curveEaseOut,
+                       animations: { toastLabel.alpha = 0.0 },
+                       completion: {(isCompleted) in toastLabel.removeFromSuperview() })
+    }
 }
 
