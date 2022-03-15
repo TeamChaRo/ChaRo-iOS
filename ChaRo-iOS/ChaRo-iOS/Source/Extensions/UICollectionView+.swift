19836ffe407feaf30b4a35c192ed9b48517c52bd
hryeong66 <jhr110326@gmail.com>
2022-03-16 00:55:22 +0900

    ✨[FEAT] #196 - collection view extension 함수 추가

--- ChaRo-iOS/ChaRo-iOS/Source/Extensions/UICollectionView+.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Extensions/UICollectionView+.swift
@@ -12,4 +12,40 @@ extension UICollectionView {
         let xib = UINib(nibName: xibName, bundle: nil)
         self.register(xib, forCellWithReuseIdentifier: xibName)
     }
+    
+    func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T {
+        guard let cell = self.dequeueReusableCell(withReuseIdentifier: T.className,
+                                                  for: indexPath) as? T else {
+            fatalError("Could not find cell with reuseID \(T.className)")
+        }
+        return cell
+    }
+
+    func dequeueHeaderView<T: UICollectionReusableView>(forIndexPath indexPath: IndexPath) -> T {
+        guard let view = dequeueReusableSupplementaryView(
+            ofKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: T.className,
+            for: indexPath
+        ) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.className)")
+        }
+        return view
+    }
+
+    func dequeueFooterView<T: UICollectionReusableView>(forIndexPath indexPath: IndexPath) -> T {
+        guard let view = dequeueReusableSupplementaryView(
+            ofKind: UICollectionView.elementKindSectionFooter,
+            withReuseIdentifier: T.className,
+            for: indexPath
+        ) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.className)")
+        }
+        return view
+    }
+
+    func register<T: UICollectionViewCell>(cell: T.Type,
+                                           forCellWithReuseIdentifier reuseIdentifier: String = T.className) {
+        register(cell, forCellWithReuseIdentifier: reuseIdentifier)
+    }
+
 }
