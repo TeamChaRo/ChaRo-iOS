34613d1d41579dfcc0cae8068a61a48900ca1b78
hryeong66 <jhr110326@gmail.com>
2022-03-15 16:40:52 +0900

    ✨[FEAT] #196 - 주의사항 디자인 변경 완료

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/PostDetailScene/PostAttentionTVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/PostDetailScene/PostAttentionTVC.swift
@@ -10,132 +10,66 @@ import SnapKit
 
 class PostAttentionTVC: UITableViewCell {
 
-    static let identifier = "PostAttentionTVC"
-    private let deviceWidthRate = UIScreen.main.bounds.width / 375
-    private let deviceHeightRate = UIScreen.main.bounds.height / 812
     public var attentionList = [false, false, false, false]
-    
-    
     private let titleView = PostCellTitleView(title: "주의사항")
 
-    private var buttonList : [UIButton] = []
-    
-    private var highwayButton: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage(named: "postAttentionUnselected"), for: .normal)
-        button.setTitle("고속도로", for: .normal)
-        button.titleLabel?.font = UIFont.notoSansMediumFont(ofSize: 14)
-        button.setTitleColor(.gray40, for: .normal)
-        
-        return button
-    }()
-   
-    private var mountainButton: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage(named: "postAttentionUnselected"), for: .normal)
-        button.setTitle("산길포함", for: .normal)
-        button.titleLabel?.font = UIFont.notoSansMediumFont(ofSize: 14)
-        button.setTitleColor(.gray40, for: .normal)
-        button.imageView?.contentMode = .scaleAspectFill
-        return button
-    }()
-    
-    private var beginnerButton: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage(named: "postAttentionUnselected"), for: .normal)
-        button.setTitle("초보힘듦", for: .normal)
-        button.titleLabel?.font = UIFont.notoSansMediumFont(ofSize: 14)
-        button.setTitleColor(.gray40, for: .normal)
-        button.imageView?.contentMode = .scaleAspectFill
-        return button
-    }()
-    
-    private var peopleButton: UIButton = {
-        let button = UIButton()
-        button.setBackgroundImage(UIImage(named: "postAttentionUnselected"), for: .normal)
-        button.setTitle("사람많음", for: .normal)
-        button.titleLabel?.font = UIFont.notoSansMediumFont(ofSize: 14)
-        button.setTitleColor(.gray40, for: .normal)
-        return button
-    }()
-    
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        setButtonList()
-        configureLayout()
+    private let attentionStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 8
+        $0.distribution = .fillEqually
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
+    
+    private let highwayButton = PostDetailContentButton()
+    private let mountainButton = PostDetailContentButton()
+    private let beginnerButton = PostDetailContentButton()
+    private let peopleButton = PostDetailContentButton()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupConstraints()
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     public func setAttentionList(list: [Bool]){
-        for i in 0..<list.count{
-            if list[i] {
-                changeToActiveButton(button: buttonList[i])
-                buttonList[i].isUserInteractionEnabled = false
-            }
-        }
+        if list.count != 4 { return }
+        highwayButton.setContent(title: "고속도로", isSelected: list[0])
+        mountainButton.setContent(title: "산길포함", isSelected: list[1])
+        beginnerButton.setContent(title: "초보힘듦", isSelected: list[2])
+        peopleButton.setContent(title: "사람많음", isSelected: list[3])
     }
 
-    private func setButtonList(){
-        buttonList.append(contentsOf: [highwayButton,
-                                       mountainButton,
-                                       beginnerButton,
-                                       peopleButton])
+    private func setupConstraints() {
+        contentView.addSubviews([titleView, attentionStackView])
+        
+        titleView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().inset(20)
+            $0.height.equalTo(22)
+        }
+        
+        attentionStackView.snp.makeConstraints {
+            $0.top.equalTo(titleView.snp.bottom).offset(12)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(92)
+        }
+        attentionStackView.addArrangedSubview(getHorizentalStackView(subViews: [highwayButton, mountainButton]))
+        attentionStackView.addArrangedSubview(getHorizentalStackView(subViews: [beginnerButton, peopleButton]))
+    }
+
+    private func configureUI() {
+        selectionStyle = .none
     }
     
-    private func changeToActiveButton(button: UIButton){
-        button.setTitleColor(.mainBlue, for: .normal)
-        button.setBackgroundImage(UIImage(named: "postAttentionSelected"), for: .normal)
+    private func getHorizentalStackView(subViews: [UIView]) -> UIStackView{
+        let stackView = UIStackView(arrangedSubviews: subViews)
+        stackView.axis = .horizontal
+        stackView.spacing = 6
+        stackView.distribution = .fillEqually
+        return stackView
     }
-    
-    private func configureLayout(){
-        addSubviews([titleView,
-                     highwayButton,
-                     mountainButton,
-                     beginnerButton,
-                     peopleButton])
-        
-        let customGap = Int(181*deviceWidthRate)
-        
-        titleView.snp.makeConstraints{make in
-            make.top.equalTo(self.snp.top)
-            make.leading.equalTo(self.snp.leading).offset(20)
-            make.trailing.equalTo(self.snp.trailing).offset(-20)
-            make.height.equalTo(22)
-        }
-        
-        highwayButton.snp.makeConstraints{make in
-            make.top.equalTo(titleView.snp.bottom)
-            make.leading.equalTo(self.snp.leading).offset(7)
-            make.trailing.equalTo(self.snp.trailing).offset(-customGap)
-            make.height.equalTo(65)
-        }
-        
-        mountainButton.snp.makeConstraints{make in
-            make.top.equalTo(titleView.snp.bottom)
-            make.leading.equalTo(self.snp.leading).offset(customGap)
-            make.trailing.equalTo(self.snp.trailing).offset(-7)
-            make.height.equalTo(65)
-        }
-        
-        beginnerButton.snp.makeConstraints{make in
-            make.leading.equalTo(self.snp.leading).offset(7)
-            make.trailing.equalTo(self.snp.trailing).offset(-customGap)
-            make.bottom.equalTo(self.snp.bottom).offset(-21)
-            make.height.equalTo(65)
-        }
-        
-        peopleButton.snp.makeConstraints{make in
-            make.leading.equalTo(self.snp.leading).offset(customGap)
-            make.trailing.equalTo(self.snp.trailing).offset(-7)
-            make.bottom.equalTo(self.snp.bottom).offset(-21)
-            make.height.equalTo(65)
-        }
-    }
-    
 }
 
