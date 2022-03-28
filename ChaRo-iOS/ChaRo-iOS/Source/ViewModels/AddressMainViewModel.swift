dfeccedae6af01650f4e198b2b7c841f4394bc04
hryeong66 <jhr110326@gmail.com>
2022-03-29 03:58:07 +0900

    ✨[FEAT] #151 - 출발지, 도착지, 경유지 추가되는 로직 구현

--- ChaRo-iOS/ChaRo-iOS/Source/ViewModels/AddressMainViewModel.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/ViewModels/AddressMainViewModel.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+import RxSwift
+import TMapSDK
+
+class AddressMainViewModel {
+    
+    var addressList: [AddressDataModel] = []
+    var searchHistory: [KeywordResult] = []
+    var newSearchHistory: [KeywordResult] = []
+    
+    let addressSubject = ReplaySubject<[(AddressDataModel, Int)]>.create(bufferSize: 1)
+    let searchHistorySubject = PublishSubject<[KeywordResult]>()
+    
+    init() {
+        addressList = [AddressDataModel(), AddressDataModel()]
+        addressSubject.onNext(refineAddressData())
+    }
+    
+    struct Input {
+        let updateAddressSubject: PublishSubject<(AddressDataModel, Int)>
+    }
+    
+    struct Output {
+        let addressSubject: ReplaySubject<[(AddressDataModel, Int)]>
+    }
+    
+    func transform(of input: Input, disposeBag: DisposeBag) -> Output {
+        input.updateAddressSubject
+            .bind(onNext: { address, index in
+            self.updateAddress(of: address, at: index)
+            }).disposed(by: disposeBag)
+        let output = Output(addressSubject: addressSubject)
+        return output
+    }
+    
+    private func updateAddress(of address: AddressDataModel, at index: Int) {
+        if address.title == "" {
+            index == 1 ? addressList.insert(address, at: 1) : addressList.remove(atOffsets: IndexSet(1...1))
+        } else {
+            addressList[index] = address
+        }
+        addressSubject.onNext(refineAddressData())
+    }
+    
+    private func refineAddressData() -> [(AddressDataModel, Int)] {
+        var list: [(AddressDataModel, Int)] = []
+        addressList.forEach { list.append(($0, addressList.count)) }
+        return list
+    }
+}
