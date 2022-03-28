bced4d8a9a60dfbd39761c5054fed0652e7e74ed
hryeong66 <jhr110326@gmail.com>
2022-03-29 03:59:10 +0900

    ✅[CHORE] #151 - 서버 코드 view model로 이동

--- ChaRo-iOS/ChaRo-iOS/Source/ViewModels/AddressMainViewModel.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/ViewModels/AddressMainViewModel.swift
@@ -55,3 +55,66 @@ class AddressMainViewModel {
         return list
     }
 }
+
+// MARK: - Network
+extension AddressMainViewModel {
+    private func getSearchKeywords() {
+        SearchKeywordService.shared.getSearchKeywords(userId: Constants.userId) { response in
+            switch(response) {
+            case .success(let resultData):
+                if let data = resultData as? SearchResultDataModel {
+                    print("내가 검색한 결과 조회~~~!!")
+                    //self.searchHistory = data.data!
+                    //dump(self.searchHistory)
+                }
+            case .requestErr(let message):
+                print("requestErr", message)
+            case .pathErr:
+                print("pathErr")
+            case .serverErr:
+                print("serverErr")
+            case .networkFail:
+                print("networkFail")
+            }
+        }
+    }
+    
+    
+    private func postSearchKeywords() {
+        print("postSearchKeywords-----------------")
+        var searchKeywordList: [SearchHistory] = []
+        
+        for item in newSearchHistory {
+            let address = SearchHistory(title: item.title,
+                                        address: item.address,
+                                        latitude: item.latitude,
+                                        longitude: item.longitude)
+            searchKeywordList.append(address)
+        }
+        
+        SearchKeywordService.shared.postSearchKeywords(userId: Constants.userId,
+                                                       keywords: searchKeywordList) {  response in
+            print("결과받음")
+            
+            switch(response) {
+            case .success(let resultData):
+                print("--------------------------")
+                print("\(resultData)")
+                print("--------------------------")
+                if let data = resultData as? SearchResultDataModel {
+                    print("성공했더~!!!!")
+                    dump(data)
+                    print("성공했더~!!!!")
+                }
+            case .requestErr(let message):
+                print("requestErr", message)
+            case .pathErr:
+                print("pathErr")
+            case .serverErr:
+                print("serverErr")
+            case .networkFail:
+                print("networkFail")
+            }
+        }
+    }
+}
