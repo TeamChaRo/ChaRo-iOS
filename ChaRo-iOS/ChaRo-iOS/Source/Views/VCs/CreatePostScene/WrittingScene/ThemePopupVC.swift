26f326b8117b9e09879e6e258ae3c4e2facd3809
hwangJi-dev <melon2228@swu.ac.kr>
2022-03-29 14:59:49 +0900

    ✅ [CHORE] : #203 - 작성하기 테마선택 로직 수정

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/CreatePostScene/WrittingScene/ThemePopupVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/CreatePostScene/WrittingScene/ThemePopupVC.swift
@@ -9,147 +9,143 @@ import UIKit
 import SnapKit
 import Then
 
+enum ThemeRemoveCase {
+    case removeFront
+    case removeLast
+}
+
 class ThemePopupVC: UIViewController {
     
-    static let identifier = "ThemePopupVC"
+    // MARK: IBOutlet
+    @IBOutlet var themeBtnItems: [UIButton]!
+    @IBOutlet var topTitleLabel: UILabel!
+    @IBOutlet var topView: UIView!
     
-    @IBOutlet var themeItemBtns: [UIButton]!
-    @IBOutlet var themeTitleLabel: UILabel!
-    @IBOutlet var themeTopView: UIView!
-    
-    private var onlineStatusDotView: UIView!
-    private var isAllThemeSeleted: Bool = false
-    private var isPresenting = false
+    // MARK: Variables
     private var filterData = FilterDatas()
-    private var currentList: [String] = []
-    private var themeList: [Int : String] = [1 : "", 2 : "", 3 : ""]
-    private var themeSelectedList: [String] = ["","",""]
+    private var themeList: [String] = []
+    private var senderIndexList: [Int] = []
+    private var removeCase: ThemeRemoveCase?
     
-    //MARK: LifeCycles
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        customThemeTopView()
-        customThemeItemBtns()
+        configureTopView()
+        configureThemeBtns()
     }
     
-    //MARK: Custom Func
-    /// setSelectedIndex - Theme 선택시 3개 딕셔너리의 키 값 유무에 따라 테마 선택 순서 지정하는 함수
-    func setSelectedIndex(senderIdx: Int) {
-        
-        if themeList[1] == "" {
-            isAllThemeSeleted = false
-            themeList[1] = themeItemBtns[senderIdx].titleLabel?.text ?? ""
-            insertSelectedImageView(sender: senderIdx, selectedNum: 1)
-        }
-        else if themeList[1] == "" && senderIdx == 15 {
-            insertSelectedImageView(sender: senderIdx, selectedNum: 1)
-        }
-        else if themeList[2] == "" {
-            isAllThemeSeleted = false
-            themeList.updateValue(themeItemBtns[senderIdx].titleLabel?.text ?? "", forKey: 2)
-            insertSelectedImageView(sender: senderIdx, selectedNum: 2)
-        }
-        else if themeList[2] == "" && senderIdx == 15 {
-            insertSelectedImageView(sender: senderIdx, selectedNum: 2)
-        }
-        else if themeList[3] == "" {
-            isAllThemeSeleted = true
-            themeList.updateValue(themeItemBtns[senderIdx].titleLabel?.text ?? "", forKey: 3)
-            insertSelectedImageView(sender: senderIdx, selectedNum: 3)
-        }
-        else if themeList[3] == "" && senderIdx == 15 {
-            insertSelectedImageView(sender: senderIdx, selectedNum: 3)
-        }
-    }
-    
-    /// removeUnselectedIndex - Theme 선택시 3개 딕셔너리의 키 값 유무에 따라 선택된 테마 값을 remove하는 함수
-    func removeUnselectedIndex(senderIdx: Int) {
-        
-        for (key, value) in themeList {
-            if value == themeItemBtns[senderIdx].titleLabel?.text {
-                themeList[key] = ""
-                removeSelectedImageView(sender: senderIdx)
-                isAllThemeSeleted = false
-            }
-        }
-    }
-    
-    //MARK: IBAction
+    // MARK: IBAction
     @IBAction func dismissBtnDidTap(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func themeBtnDidTap(_ sender: UIButton) {
-        
         if sender.isSelected {
-            removeUnselectedIndex(senderIdx: sender.tag)
-            themeItemBtns[sender.tag].backgroundColor = .white
-            themeItemBtns[sender.tag].layer.borderColor = UIColor.gray20.cgColor
-            sender.isSelected = false
-        }
-        else {
-            if themeItemBtns[15].isSelected || isAllThemeSeleted == true {
-                //더 이상 선택 불가 alert 띄우기
-                themeItemBtns[sender.tag].isSelected = false
+            let removeIndex = senderIndexList.firstIndex(where: {$0 == sender.tag}) ?? 0
+            configureThemeBtnsBySelect(selectedBtn: sender, isSelected: false, senderTag: sender.tag)
+            removeCase = senderIndexList.count == removeIndex + 1 ? .removeLast : .removeFront
+            removeSelectedIndexImageView(senderTag: sender.tag, removeCase: removeCase ?? .removeFront)
+            themeList.remove(at: removeIndex)
+            senderIndexList.remove(at: removeIndex)
+            
+            // removeCase: removeFront의 경우 -> 현재 list에 맞는 index를 새로 add
+            if !senderIndexList.isEmpty && removeCase == .removeFront {
+                for i in 0...senderIndexList.count - 1 {
+                    addSelectedIndexImageView(senderTag: senderIndexList[i], selectedIndex: i)
+                }
             }
-            else {
-                themeItemBtns[sender.tag].backgroundColor = .blueSelect
-                themeItemBtns[sender.tag].layer.borderColor = UIColor.mainBlue.cgColor
-                themeItemBtns[sender.tag].setTitleColor(UIColor.mainBlue, for: .selected)
-                sender.isSelected = true
-                setSelectedIndex(senderIdx: sender.tag)
+        } else {
+            if themeList.count == 3 || themeList.contains("선택안함") {
+                // 테마 최대 선택 개수에 도달하거나, "선택안함"이 이미 선택되어 있는 경우
+                themeBtnItems[sender.tag].isSelected = false
+                // TODO: Alert 띄우기
+            } else {
+                configureThemeBtnsBySelect(selectedBtn: sender, isSelected: true, senderTag: sender.tag)
+                themeList.append(themeBtnItems[sender.tag].titleLabel?.text ?? "")
+                senderIndexList.append(sender.tag)
+                addSelectedIndexImageView(senderTag: sender.tag, selectedIndex: senderIndexList.firstIndex(where: {$0 == sender.tag}) ?? 0)
             }
         }
     }
     
     @IBAction func confirmThemeBtn(_ sender: UIButton) {
-        let sortedDict = themeList.sorted { $0.0 < $1.0 }
-        themeSelectedList = Array(sortedDict.map({ $0.value }))
+        var passList: [String] = themeList
+        
+        if passList.isEmpty {
+            // TODO: Alert 띄우기
+            print("Alert")
+        } else {
+            if passList.count == 1 {
+                passList.append(contentsOf: ["선택안함", "선택안함"])
+            } else if passList.count == 2 {
+                passList.append("선택안함")
+            }
+            print(passList)
+        }
     }
 }
 
 //MARK: - UI
 extension ThemePopupVC {
-    func customThemeTopView() {
-        themeTitleLabel.font = .notoSansMediumFont(ofSize: 17)
-        themeTopView.layer.cornerRadius = 21
-        themeTopView.clipsToBounds = true
-        themeTopView.layer.maskedCorners = [ .layerMaxXMinYCorner, .layerMinXMinYCorner]
+    private func configureTopView() {
+        topTitleLabel.font = .notoSansMediumFont(ofSize: 17)
+        topView.layer.cornerRadius = 21
+        topView.clipsToBounds = true
+        topView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
     }
     
-    func customThemeItemBtns() {
-        themeItemBtns.forEach({
+    /// 테마 아이템 버튼들의 첫 UI를 구성하는 함수
+    private func configureThemeBtns() {
+        themeBtnItems.forEach({
             $0.layer.cornerRadius = 21
             $0.layer.borderWidth = 1
             $0.layer.borderColor = UIColor.gray20.cgColor
             $0.tintColor = .clear
             $0.setTitleColor(UIColor.gray40, for: .normal)
+            $0.setTitleColor(UIColor.mainBlue, for: .selected)
             $0.titleLabel?.font = .notoSansMediumFont(ofSize: 14)
         })
         
-        currentList = filterData.thema.reversed()
+        let entireFilterList: [String] = filterData.thema.reversed()
         
-        for i in 0...currentList.count - 1 {
-            themeItemBtns[i].setTitle(currentList[i], for: .normal)
+        for i in 0...entireFilterList.count - 1 {
+            themeBtnItems[i].setTitle(entireFilterList[i], for: .normal)
         }
     }
     
-    /// insertSelectedImageView - Theme 선택시 선택 순서 나타내는 ImageView를 addSubView하는 함수
-    func insertSelectedImageView(sender: Int, selectedNum: Int) {
-        
-        let selectNumImageView = UIImageView(frame: CGRect(x: 9, y: 13, width: 14, height:15))
-        selectNumImageView.tag = sender + 1
-        selectNumImageView.image = UIImage(named: "themeNumber\(selectedNum)")
-        selectNumImageView.clipsToBounds = true
-        themeItemBtns[sender].addSubview(selectNumImageView)
+    /// 버튼 선택상태에 따라 테마 아이템 버튼들의 UI를 구성하는 함수
+    private func configureThemeBtnsBySelect(selectedBtn: UIButton, isSelected: Bool, senderTag: Int) {
+        selectedBtn.isSelected = isSelected
+        themeBtnItems[senderTag].backgroundColor = isSelected ? .blueSelect : .white
+        themeBtnItems[senderTag].layer.borderColor = isSelected ? UIColor.mainBlue.cgColor : UIColor.gray20.cgColor
     }
     
-    /// removeSelectedImageView - Theme 선택시 선택 순서 나타내는 ImageView를 removeFromSuperView하는 함수
-    func removeSelectedImageView(sender: Int) {
+    /// Theme 선택시 선택 순서 나타내는 ImageView를 addSubView하는 함수
+    private func addSelectedIndexImageView(senderTag: Int, selectedIndex: Int) {
+        let selectIndexImageView = UIImageView(frame: CGRect(x: 9, y: 13, width: 14, height:15))
+        selectIndexImageView.tag = senderTag + 1
+        selectIndexImageView.image = UIImage(named: "themeNumber\(selectedIndex + 1)")
+        selectIndexImageView.clipsToBounds = true
+        themeBtnItems[senderTag].addSubview(selectIndexImageView)
+    }
+    
+    /// Theme 선택시 선택 순서 나타내는 ImageView를 removeFromSuperView하는 함수
+    private func removeSelectedIndexImageView(senderTag: Int, removeCase: ThemeRemoveCase) {
         
-        for subview in themeItemBtns[sender].subviews {
-            if subview.tag == sender + 1 {
-                subview.removeFromSuperview()
+        switch removeCase {
+        case .removeLast:
+            for subview in themeBtnItems[senderTag].subviews {
+                if subview.tag == senderTag + 1 {
+                    subview.removeFromSuperview()
+                }
+            }
+        case .removeFront:
+            for i in senderIndexList {
+                for subview in themeBtnItems[i].subviews {
+                    if subview.tag == i + 1 {
+                        subview.removeFromSuperview()
+                    }
+                }
             }
         }
     }
