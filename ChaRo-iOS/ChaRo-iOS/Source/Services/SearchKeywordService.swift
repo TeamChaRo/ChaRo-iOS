d68f9c29b9da9d45bebf86b5ecf50d05b8104cb0
hryeong66 <jhr110326@gmail.com>
2022-04-05 04:29:49 +0900

    ✨[FEAT] #145 - 최근 검색 기록 api 통신 연결

--- ChaRo-iOS/ChaRo-iOS/Source/Services/SearchKeywordService.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Services/SearchKeywordService.swift
@@ -28,7 +28,7 @@ struct SearchKeywordService {
                                      parameters: makeParameter(userId: userId, keywords: keywords),
                                      encoding: JSONEncoding.default,
                                      headers: header)
-        print("검색어 post!!!!!!!")
+        
         dataRequest.responseData{ dataResponse in
             switch dataResponse.result {
             case .success:
@@ -45,23 +45,34 @@ struct SearchKeywordService {
     func getSearchKeywords(userId: String,
                            completion: @escaping (NetworkResult<Any>) -> Void) {
        
-        let dataRequeat = AF.download(Constants.searchKeywordURL+"/\(userId)",
+        let dataRequeat = AF.download(Constants.searchKeywordURL,
                                       method: .get,
                                       encoding: JSONEncoding.default,
                                       headers: header)
         
-        dataRequeat.responseData{ dataResponse in
+        dataRequeat.responseData { dataResponse in
             switch dataResponse.result {
             case .success:
-                guard let statusCode = dataResponse.response?.statusCode else { return}
-                guard let value = dataResponse.value  else {return}
-                let networkResult = self.judgeStatus(by: statusCode, value)
+                guard let statusCode = dataResponse.response?.statusCode,
+                      let value = dataResponse.value else { return }
+                let networkResult = self.judgeStatus(type: [KeywordResult].self, by: statusCode, value)
                 completion(networkResult)
-                
             case .failure(_): completion(.pathErr)
             }
         }
-        
+    }
+    
+    private func judgeStatus<T: Codable>(type: T.Type, by statusCode: Int, _ data: Data) ->  NetworkResult<Any> {
+        guard let decodeData = try? JSONDecoder().decode(GenericResponse<T>.self, from: data) else {
+            return .pathErr
+        }
+        switch statusCode {
+        case 200: return .success(decodeData.data)
+        case 400: return .requestErr(decodeData)
+        case 500: return .serverErr
+        default:
+            return .networkFail
+        }
     }
     
     private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
