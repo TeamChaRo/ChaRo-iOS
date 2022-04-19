0494eec901862b66046480b5398083f4cead6e77
hryeong66 <jhr110326@gmail.com>
2022-04-19 17:38:26 +0900

    ✅[CHORE] #145 - 검색 기록 post 서버 연결 수정

--- ChaRo-iOS/ChaRo-iOS/Source/Services/SearchKeywordService.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Services/SearchKeywordService.swift
@@ -14,21 +14,17 @@ struct SearchKeywordService {
     static let shared = SearchKeywordService()
     let header: HTTPHeaders = ["Content-Type": "application/json"]
     
-    private func makeParameter(userId: String, keywords: [SearchHistory]) -> Parameters {
-        let post = SearchKeywordDataModel(userId: userId, searchHistory: keywords)
-        return  post.toJSON()
+    private func makeParameter(keywords: [SearchHistory]) -> Parameters {
+        let body = SearchKeywordDataModel(userEmail: Constants.userEmail, searchHistory: keywords)
+        return  body.toJSON()
     }
     
-    func postSearchKeywords(userId: String,
-                            keywords: [SearchHistory],
-                            completion: @escaping (NetworkResult<Any>) -> Void) {
-        
-        let dataRequest = AF.request(Constants.searchKeywordURL,
+    func postSearchKeywords(keywords: [SearchHistory], completion: @escaping (NetworkResult<Any>) -> Void) {
+        let dataRequest = AF.request(Constants.saveSearchedHistory,
                                      method: .post,
-                                     parameters: makeParameter(userId: userId, keywords: keywords),
+                                     parameters: makeParameter(keywords: keywords),
                                      encoding: JSONEncoding.default,
                                      headers: header)
-        
         dataRequest.responseData{ dataResponse in
             switch dataResponse.result {
             case .success:
@@ -36,7 +32,6 @@ struct SearchKeywordService {
                 guard let value = dataResponse.value  else {return}
                 let networkResult = self.judgeStatus(by: statusCode, value)
                 completion(networkResult)
-                
             case .failure(_): completion(.pathErr)
             }
         }
@@ -66,6 +61,7 @@ struct SearchKeywordService {
         guard let decodeData = try? JSONDecoder().decode(GenericResponse<T>.self, from: data) else {
             return .pathErr
         }
+        
         switch statusCode {
         case 200: return .success(decodeData.data)
         case 400: return .requestErr(decodeData)
@@ -76,9 +72,7 @@ struct SearchKeywordService {
     }
     
     private func judgeStatus(by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
-        let decoder = JSONDecoder()
-        
-        guard let decodeData = try? decoder.decode(SearchResultDataModel.self, from: data) else {
+        guard let decodeData = try? JSONDecoder().decode(SearchResultDataModel.self, from: data) else {
             return .pathErr
         }
         
