b4704e13dc3a9a3ba9eca3686363a914b937d98e
hryeong66 <jhr110326@gmail.com>
2022-04-19 23:08:37 +0900

    ✨[FEAT] #145 - 검색 키워드 색 파란색으로 나타나도록 구현

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/CreatePostScene/MapScene/SearchAddressKeywordVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/CreatePostScene/MapScene/SearchAddressKeywordVC.swift
@@ -87,9 +87,9 @@ final class SearchAddressKeywordVC: UIViewController {
             .bind(to: tableView.rx.items(cellIdentifier: SearchKeywordCell.className,
                                          cellType: SearchKeywordCell.self)) { [weak self] row, element, cell in
                 guard let self = self else { return }
-                cell.titleLabel.text = element.title
-                cell.addressLabel.text = element.address
-                cell.dateLabel.text = self.viewModel.getCurrentDate()
+                cell.setContent(element: element,
+                                keyword: self.searchTextField.text ?? "",
+                                date: self.viewModel.getCurrentDate())
                 self.contentTitleLabel.text = self.viewModel.isSearchedHistoryList ? "최근 검색어" : "검색 결과"
             }.disposed(by: disposeBag)
         
@@ -101,8 +101,12 @@ final class SearchAddressKeywordVC: UIViewController {
         
         searchTextField.rx.text
             .throttle(.milliseconds(500), scheduler: MainScheduler.asyncInstance)
-            .subscribe(onNext: { [weak self] in
-                self?.viewModel.findAutoCompleteAddressList(keyword: $0 ?? "")
+            .subscribe(onNext: { [weak self]  in
+                if $0 == "" { //to
+                    print("clear 버튼 클릭")
+                } else {
+                    self?.viewModel.findAutoCompleteAddressList(keyword: $0 ?? "")
+                }
             }).disposed(by: disposeBag)
         
         backButton.rx.tap
