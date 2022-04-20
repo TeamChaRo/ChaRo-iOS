d0a11e78977343bb85d58b3cc9e479a747d6766c
hryeong66 <jhr110326@gmail.com>
2022-04-20 17:19:42 +0900

    ✅[CHORE] #216 tabbarVC에서 merge 하면서 빠진 부분 수정

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/Tabbar/TabbarVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/Tabbar/TabbarVC.swift
@@ -14,6 +14,7 @@ class TabbarVC: UITabBarController {
     public var addressMainVC: AddressMainVC?
     public var tabs: [UIViewController] = []
     private var comeBackIndex = 0
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +25,38 @@ class TabbarVC: UITabBarController {
         super.viewWillAppear(animated)
         selectedViewController = tabs[comeBackIndex]
     }
+    func setBackgroundClear() {
+        tabBar.isTranslucent = true
+        tabBar.backgroundImage = UIImage()
+        tabBar.shadowImage = UIImage()
+    }
+    func setShadow() {
+        tabBar.getShadowView(color: UIColor.black.cgColor,
+                             masksToBounds: false,
+                             shadowOffset: CGSize(width: 0, height: 0),
+                             shadowRadius: 10,
+                             shadowOpacity: 0.5
+        )
+    }
+    func setRadius() {
+        tabBar.backgroundColor = .white
+        tabBar.layer.cornerRadius = 15
+        tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        tabBar.clipsToBounds = false
+    }
     
+    override func viewDidLayoutSubviews() {
+        let tabbarY = view.getDeviceHeight()
+        let tabbarX = view.getDeviceWidth()
+        var tabbarHeight = 100
+        setRadius()
+        setShadow()
+        let frame = CGRect(x: 0, y: tabbarY - tabbarHeight,
+                           width: tabbarX, height: tabbarHeight)
+        self.tabBar.frame = frame
+    }
     
     internal override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-        
-        
         if item.title == "작성하기" {
             let createStoryboard = UIStoryboard(name: "CreatePost", bundle: nil)
 
@@ -37,22 +65,19 @@ class TabbarVC: UITabBarController {
 
             createTab.modalPresentationStyle = .fullScreen
             self.present(createTab, animated: false, completion: nil)
-        } else if item.title == "나의 차로" {
+        } else if item.title == "나의차로" {
             comeBackIndex = 2
         } else {
             comeBackIndex = 0
         }
-        
         print("comeBackIndex = \(comeBackIndex)")
         
     }
     
     
     private func configTabbar() {
-        
-        let customTabbar = tabBar
-        customTabbar.tintColor = .blue
-        customTabbar.backgroundColor = UIColor.white
+        self.view.tintColor = .blue
+        self.view.backgroundColor = UIColor.white
 
         let homeStoryboard = UIStoryboard(name: "Home", bundle: nil)
         let homeVC = homeStoryboard.instantiateViewController(identifier: "HomeVC")
@@ -77,10 +102,8 @@ class TabbarVC: UITabBarController {
         
 
         tabs = [homeTab,createTab, myPageTab]
-        
         setViewControllers(tabs, animated: true)
         selectedViewController = homeTab
     }
     
 }
-
