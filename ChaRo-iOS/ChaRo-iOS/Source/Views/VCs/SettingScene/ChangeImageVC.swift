dfc367602a252affc6c30e4f068a7d5f84ba9a8d
Jen <wonee0321@gmail.com>
2022-04-26 02:02:55 +0900

    #153 [FEAT] 비밀번호, 프로필 수정 VC 생성 및 연결

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SettingScene/ChangeImageVC.swift
@@ -0,0 +1,18 @@
+//
+//  ChangeImageVC.swift
+//  ChaRo-iOS
+//
+//  Created by JEN Lee on 2022/04/26.
+//
+
+import UIKit
+
+class ChangeImageVC: UIViewController {
+
+    static let identifier = "ChangeImageVC"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SettingScene/ChangePasswordVC.swift
@@ -0,0 +1,17 @@
+//
+//  ChangePasswordVC.swift
+//  ChaRo-iOS
+//
+//  Created by JEN Lee on 2022/04/26.
+//
+
+import UIKit
+
+class ChangePasswordVC: UIViewController {
+    
+    static let identifier = "ChangePasswordVC"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
