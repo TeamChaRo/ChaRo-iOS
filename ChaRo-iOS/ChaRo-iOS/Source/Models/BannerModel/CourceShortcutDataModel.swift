e4c8d168f44a9d4e1302ee8eb4085e1d247cdefc
hryeong66 <jhr110326@gmail.com>
2022-05-03 21:46:30 +0900

    ➕[ADD] #210 자동차 극장 드라이브 코스 바로가기 component로 들어갈때 사용할 Model 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Models/BannerModel/CourceShortcutDataModel.swift
@@ -0,0 +1,14 @@
+//
+//  CourceShortcutDataModel.swift
+//  ChaRo-iOS
+//
+//  Created by 장혜령 on 2022/05/03.
+//
+
+import UIKit
+
+struct CourceShortcutDataModel {
+    let location, title, description, courseContent: String
+    let tags: [String]
+    let thumnails: [UIImage]
+}
