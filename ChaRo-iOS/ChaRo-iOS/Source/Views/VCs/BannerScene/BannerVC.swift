beb73198b17beb951780118c22e4f5de1b87ad3a
hryeong66 <jhr110326@gmail.com>
2022-05-18 01:10:26 +0900

    ✨[FEAT] #210 - 배너들의 상위 VC 구현

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/BannerScene/BannerVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/BannerScene/BannerVC.swift
@@ -5,4 +5,35 @@
 //  Created by 장혜령 on 2022/05/10.
 //
 
-import Foundation
+import UIKit
+
+import Then
+import RxSwift
+
+class BannerVC: UIViewController {
+    
+    let disposeBag = DisposeBag()
+    let viewRetio: CGFloat = UIScreen.getDeviceWidth() / 375
+    let scrollView = UIScrollView()
+    
+    override func viewDidLoad() {
+        configureUI()
+        setConstraints()
+        bind()
+    }
+    
+    func setConstraints() {
+        view.addSubview(scrollView)
+        scrollView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    func configureUI() {
+        view.backgroundColor = .white
+    }
+    
+    func bind() {
+        
+    }
+}
