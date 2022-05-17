7ea50e3abb2be036ac5bbc27db3f836d191aab92
hryeong66 <jhr110326@gmail.com>
2022-05-18 01:11:02 +0900

    ✨[FEAT] #210 - 첫번째배너(강릉 코스) 구현

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/BannerScene/FirstBannerVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/BannerScene/FirstBannerVC.swift
@@ -5,4 +5,26 @@
 //  Created by 장혜령 on 2022/05/10.
 //
 
-import Foundation
+import UIKit
+
+import SnapKit
+import Then
+
+final class FirstBannerVC: BannerVC {
+    
+    let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+        $0.image = ImageLiterals.imgGangneungCourse
+    }
+    
+    override func setConstraints() {
+        super.setConstraints()
+        scrollView.addSubview(imageView)
+        imageView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(imageView.frame.height * viewRetio)
+        }
+    }
+}
