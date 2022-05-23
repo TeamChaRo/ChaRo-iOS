01fed54cdf294a420d159b393a0220c0acc3192d
hryeong66 <jhr110326@gmail.com>
2022-05-23 23:48:14 +0900

    ✨[FEAT] #210 - 상단 back button, titleLabel 추가

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/BannerScene/BannerVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/BannerScene/BannerVC.swift
@@ -16,6 +16,23 @@ class BannerVC: UIViewController {
     let viewRetio: CGFloat = UIScreen.getDeviceWidth() / 375
     let scrollView = UIScrollView()
     
+    private let backButton = UIButton().then {
+        $0.setImage(ImageLiterals.icBack, for: .normal)
+    }
+    private let titleLabel = UILabel().then {
+        $0.font = .notoSansMediumFont(ofSize: 17)
+        $0.textColor = .subBlack
+    }
+    
+    init(title: String) {
+        titleLabel.text = title
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         configureUI()
         setConstraints()
@@ -23,9 +40,21 @@ class BannerVC: UIViewController {
     }
     
     func setConstraints() {
-        view.addSubview(scrollView)
+        view.addSubviews([backButton, titleLabel, scrollView])
+        backButton.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.equalToSuperview()
+            $0.width.height.equalTo(48)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.centerY.equalTo(backButton.snp.centerY)
+            $0.centerX.equalToSuperview()
+        }
+        
         scrollView.snp.makeConstraints {
-            $0.top.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+            $0.top.equalTo(backButton.snp.bottom).offset(9)
+            $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
         }
     }
     
@@ -34,6 +63,9 @@ class BannerVC: UIViewController {
     }
     
     func bind() {
-        
+        backButton.rx.tap.asDriver()
+            .drive(onNext: { [weak self] in
+                self?.dismiss(animated: true)
+        }).disposed(by: disposeBag)
     }
 }
