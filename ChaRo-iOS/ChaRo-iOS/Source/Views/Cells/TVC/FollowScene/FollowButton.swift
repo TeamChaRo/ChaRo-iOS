c8dada1a6a9cf640aa6a05688bf26180c0b34dea
hryeong66 <jhr110326@gmail.com>
2022-06-25 05:13:12 +0900

    ✨[FEAT] #237 - follow button 구현

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/FollowScene/FollowButton.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/FollowScene/FollowButton.swift
@@ -5,4 +5,33 @@
 //  Created by 장혜령 on 2022/06/25.
 //
 
-import Foundation
+import UIKit
+
+final class FollowButton: UIButton {
+    
+    override var isSelected: Bool {
+        didSet {
+            self.layer.borderColor = isSelected ? UIColor.mainBlue.cgColor : UIColor.gray30.cgColor
+            self.backgroundColor = isSelected ? .mainBlue : .white
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureUI() {
+        self.layer.cornerRadius = 11
+        self.layer.borderWidth = 1
+        self.setTitleColor(.white, for: .selected)
+        self.setTitleColor(.gray30, for: .normal)
+        self.setTitle("팔로우" , for: .selected)
+        self.setTitle("팔로잉", for: .normal)
+        self.titleLabel?.font = UIFont.notoSansMediumFont(ofSize: 13)
+    }
+}
