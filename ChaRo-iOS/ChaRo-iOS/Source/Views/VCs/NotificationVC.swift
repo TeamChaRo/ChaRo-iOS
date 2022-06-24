46da1d048cbc89fcf743d37ec5d604012ec5b20b
hwangJi-dev <melon2228@swu.ac.kr>
2022-06-25 00:28:36 +0900

    ✅[CHORE] #234 - 새로 온 알림을 클릭했을 때 서버 연결, 읽음 처리 구현

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/NotificationVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/NotificationVC.swift
@@ -60,13 +60,26 @@ extension NotificationVC: UITableViewDataSource {
 
 // MARK: - UITableViewDelegate
 extension NotificationVC: UITableViewDelegate {
+    
+    /// heightForRowAt
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 97
+    }
+    
+    /// didSelectRowAt
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        // isRead: 0 - 읽지 않음 // isRead: 1 - 읽음
+        if notificationList[indexPath.row].isRead == 0 {
+            readNotificationData(pushId: notificationList[indexPath.row].pushID ?? -1)
+        }
     }
 }
 
 // MARK: - Network
 extension NotificationVC {
+   
+    /// 알림 목록 조회를 요청하는 메서드
     private func getNotificationListData() {
         NotificationService.shared.getNotificationList { response in
             switch(response) {
@@ -75,6 +88,24 @@ extension NotificationVC {
                     self.notificationList = data
                     self.notificationListTV.reloadData()
                 }
+            case .requestErr(let message):
+                print("requestErr", message)
+            case .pathErr:
+                print("pathErr")
+            case .serverErr:
+                print("serverErr")
+            case .networkFail:
+                print("networkFail")
+            }
+        }
+    }
+    
+    /// 새로 온 알림을 클릭했을 때 '읽음'요청을 보내는 메서드
+    private func readNotificationData(pushId: Int) {
+        NotificationService.shared.postNotificationIsRead(pushId: pushId) { response in
+            switch(response) {
+            case .success(_):
+                print("success!")
             case .requestErr(let message):
                 print("requestErr", message)
             case .pathErr:
