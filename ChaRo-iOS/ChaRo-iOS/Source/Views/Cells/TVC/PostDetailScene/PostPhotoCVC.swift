b000c9b73885245adf3883d3cd28529777c4a2e1
hryeong66 <jhr110326@gmail.com>
2022-06-25 07:16:49 +0900

    ✅[CHORE] #248 - 이미지 pin 체스쳐와 index cell에서 적용 되도록 수정

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/PostDetailScene/PostPhotoCVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/PostDetailScene/PostPhotoCVC.swift
@@ -10,7 +10,7 @@ import SnapKit
 import Then
 
 final class PostPhotoCVC: UICollectionViewCell{
-
+    
     let imageView = UIImageView().then {
         $0.contentMode = .scaleAspectFill
     }
@@ -18,6 +18,7 @@ final class PostPhotoCVC: UICollectionViewCell{
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupContraints()
+        
     }
     
     required init?(coder: NSCoder) {
@@ -29,10 +30,28 @@ final class PostPhotoCVC: UICollectionViewCell{
         imageView.snp.makeConstraints{
             $0.top.leading.trailing.bottom.equalToSuperview()
         }
+        setupGesture()
     }
     
     func setImage(to imgString: String) {
         imageView.kf.setImage(with: URL(string: imgString))
+    }
+    
+    private func setupGesture() {
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinchGesture(gesture:)))
+        self.contentView.addGestureRecognizer(pinchGesture)
+    }
+    
+    @objc
+    private func handlePinchGesture(gesture: UIPinchGestureRecognizer) {
+        switch gesture.state {
+        case .changed:
+            imageView.transform = imageView.transform.scaledBy(x: gesture.scale, y: gesture.scale)
+            gesture.scale = 1
+        case .ended:
+            imageView.transform = CGAffineTransform.identity
+        default: print("")
+        }
     }
     
 }
