0f30781f7f7757e9b1a98bf2d9af65593003f957
hryeong66 <jhr110326@gmail.com>
2022-06-25 00:04:26 +0900

    ✅[CHORE] #233 - expended imgae view에 들어갈 파일들 추가

---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/CVC/PostDetailScene/PostDetailPhotoCVC.swift
@@ -0,0 +1,8 @@
+//
+//  PostDetailPhotoCVC.swift
+//  ChaRo-iOS
+//
+//  Created by 장혜령 on 2022/06/24.
+//
+
+import Foundation
---
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/PostDetailScene/ExpendedImageVC.swift
@@ -0,0 +1,8 @@
+//
+//  ExpendedImageVC.swift
+//  ChaRo-iOS
+//
+//  Created by 장혜령 on 2022/06/24.
+//
+
+import Foundation
