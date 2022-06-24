cb7ad3dc1a3722c1626caa2de0497eeea238ac19
hryeong66 <jhr110326@gmail.com>
2022-06-25 00:07:55 +0900

    ✅[CHORE] #233 - PostDetailPhotoCVC 임시 추가

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/CVC/PostDetailScene/PostDetailPhotoCVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/CVC/PostDetailScene/PostDetailPhotoCVC.swift
@@ -5,4 +5,36 @@
 //  Created by 장혜령 on 2022/06/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+import Kingfisher
+
+final class PostDetailPhotoCVC: UICollectionViewCell {
+    
+    // MARK: - properties
+    let imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        render()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func render() {
+        addSubview(imageView)
+        imageView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+    
+    func setImage(to url: String) {
+        let url = URL(string: url)
+        imageView.kf.setImage(with: url)
+    }
+}
