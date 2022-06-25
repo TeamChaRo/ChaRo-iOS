8417d69f321edf84e73e75558b9521583d53a917
hwangJi-dev <melon2228@swu.ac.kr>
2022-06-25 20:37:49 +0900

    ♻️[REFACTOR]: #245 - SettingVC(설정뷰) 코드 리펙토링

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SettingScene/SettingVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SettingScene/SettingVC.swift
@@ -2,7 +2,7 @@
 //  SettingVC.swift
 //  ChaRo-iOS
 //
-//  Created by 박익범 on 2021/12/29.
+//  Created by hwangJi on 2022/06/25.
 //
 
 import UIKit
@@ -14,107 +14,142 @@ import PhotosUI
 
 final class SettingVC: UIViewController {
     
-    // MARK: Variable
-    private let userWidth = UIScreen.main.bounds.width
-    private let userheight = UIScreen.main.bounds.height
-    
-    private var permissionModel = [settingDataModel(titleString: "사진", isToggle: true, toggleData: true)]
-    
-    private var accountModel = [settingDataModel(titleString: "프로필 수정", titleLabelColor: UIColor.black),
-                                settingDataModel(titleString: "비밀번호 수정", titleLabelColor: UIColor.black),
-                                settingDataModel(titleString: "이메일", titleLabelColor: UIColor.black, isSubLabel: true, subLabelString: "\(UserDefaults.standard.object(forKey: Constants.UserDefaultsKey.userEmail) ?? "ios@gmail.com")", subLabelColor: UIColor.black)]
-    
-    private var infoInquiryModel = [settingDataModel(titleString: "공지사항", titleLabelColor: UIColor.black),
-                                    settingDataModel(titleString: "1:1 문의", titleLabelColor: UIColor.black),
-                                    settingDataModel(titleString: "신고하기", titleLabelColor: UIColor.black)]
-    private var termsModel = [settingDataModel(titleString: "개인정보 처리방침", titleLabelColor: UIColor.black),
-                              settingDataModel(titleString: "서비스 이용약관", titleLabelColor: UIColor.black),
-                              settingDataModel(titleString: "오픈소스 라이선스", titleLabelColor: UIColor.black),
-                              settingDataModel(titleString: "버전 정보", titleLabelColor: UIColor.gray30, isSubLabel: true, subLabelString: Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "", subLabelColor: UIColor.gray30),
-                              settingDataModel(titleString: "로그아웃", titleLabelColor: UIColor.mainBlue),
-                              settingDataModel(titleString: "회원탈퇴", titleLabelColor: UIColor.mainOrange)]
-    
-    // headerView
-    private let settingBackgroundView = UIView().then {
+    // MARK: Properties
+    private let naviBarView = UIView().then {
         $0.backgroundColor = UIColor.white
     }
     
-    private let headerTitleLabel = UILabel().then {
+    private let naviTitleLabel = UILabel().then {
         $0.text = "설정"
-        $0.font = UIFont.notoSansRegularFont(ofSize: 17)
         $0.textColor = UIColor.black
+        $0.font = UIFont.notoSansRegularFont(ofSize: 17)
     }
     
     private let backButton = UIButton().then {
         $0.setBackgroundImage(UIImage(named: "backIcon"), for: .normal)
-        $0.addTarget(self, action: #selector(backButtonClicked(_:)), for: .touchUpInside)
     }
     
-    private let bottomView = UIView().then {
+    private let naviBottomView = UIView().then {
         $0.backgroundColor = UIColor.gray20
     }
     
-    // tableView
     private let settingTableView: UITableView = {
         let tableView = UITableView(frame: CGRect.zero, style: .grouped)
         tableView.backgroundColor = UIColor.white
         return tableView
     }()
     
-    // MARK: ViewDidLoad
+    // MARK: Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        setHeaderLayout()
-        setTableViewLayout()
-        addNotificationObserver()
+        configureUI()
+        configureTableView()
+        configureButtonAddTarget()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.tabBarController?.tabBar.isHidden = true
     }
+}
+
+// MARK: - Configure
+
+extension SettingVC {
     
-    // MARK: Function, ServerFunction, LayoutFunction
-    private func setHeaderLayout() {
-        let headerHeigth = userheight * 0.15
-        self.view.addSubview(settingBackgroundView)
-        settingBackgroundView.addSubviews([headerTitleLabel, backButton, bottomView])
+    // MARK: UI
+    private func configureUI() {
+        view.addSubviews([naviBarView, settingTableView])
+        naviBarView.addSubviews([naviTitleLabel, backButton, naviBottomView])
+
+        naviBarView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(UIScreen.hasNotch ? UIScreen.getNotchHeight() + 58: 93)
+        }
         
-        settingBackgroundView.snp.makeConstraints {
-            $0.top.leading.trailing.equalToSuperview().offset(0)
-            $0.height.equalTo(headerHeigth)
+        naviTitleLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(25)
+            $0.centerX.equalTo(naviBarView)
         }
-        headerTitleLabel.snp.makeConstraints {
-            $0.centerX.equalTo(settingBackgroundView)
-            $0.bottom.equalToSuperview().offset(-25)
-            $0.width.equalTo(32)
-        }
+        
         backButton.snp.makeConstraints {
             $0.width.height.equalTo(48)
-            $0.leading.equalToSuperview().offset(0)
-            $0.centerY.equalTo(headerTitleLabel)
+            $0.leading.equalToSuperview()
+            $0.centerY.equalTo(naviTitleLabel)
         }
-        bottomView.snp.makeConstraints {
-            $0.leading.trailing.bottom.equalToSuperview().offset(0)
+        
+        naviBottomView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
             $0.height.equalTo(1)
         }
         
+        settingTableView.snp.makeConstraints {
+            $0.top.equalTo(naviBarView.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
     }
     
-    private func setTableViewLayout() {
-        self.view.addSubview(settingTableView)
-        settingTableView.separatorStyle = .none
+    // MARK: Button
+    private func configureButtonAddTarget() {
+        backButton.addTarget(self, action: #selector(backButtonClicked(_:)), for: .touchUpInside)
+    }
+    
+    // MARK: TableView
+    private func configureTableView() {
         settingTableView.delegate = self
         settingTableView.dataSource = self
-        settingTableView.registerCustomXib(xibName: "SettingTVC")
-        
-        settingTableView.snp.makeConstraints {
-            $0.top.equalTo(settingBackgroundView.snp.bottom).offset(0)
-            $0.leading.trailing.bottom.equalToSuperview().offset(0)
+        settingTableView.registerCustomXib(xibName: SettingTVC.identifier)
+        settingTableView.separatorStyle = .none
+    }
+    
+    // MARK: TableView Header
+    private func configureHeaderView(section: Int) -> UIView {
+        let headerView = UIView().then {
+            $0.backgroundColor = UIColor.white
         }
+        
+        let bottomView = UIView().then {
+            $0.backgroundColor = UIColor.gray20
+        }
+        
+        let titleLabel = UILabel().then {
+            $0.font = UIFont.notoSansRegularFont(ofSize: 12)
+            $0.textColor = UIColor.gray50
+            $0.textAlignment = .left
+        }
+        
+        headerView.addSubviews([titleLabel, bottomView])
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(18)
+            $0.leading.equalToSuperview().offset(19)
+            $0.width.equalTo(150)
+        }
+        
+        bottomView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(1)
+        }
+        
+        switch SettingSection.allCases[section] {
+        case .accessAllow:
+            titleLabel.text = "접근허용"
+            bottomView.backgroundColor = UIColor.white
+        case .account:
+            titleLabel.text = "계정"
+        case .info:
+            titleLabel.text = "정보"
+        case .serviceCenter:
+            titleLabel.text = "고객센터"
+        case .terms:
+            titleLabel.text = "약관"
+        }
+        
+        return headerView
     }
 }
 
 // MARK: - Custom Methods
+
 extension SettingVC {
     
     /// SafariViewController를 불러와 화면전환을 하는 메서드 (인앱)
@@ -122,11 +157,6 @@ extension SettingVC {
         guard let url = URL(string: urlString) else { return }
         let safariView: SFSafariViewController = SFSafariViewController(url: url)
         self.present(safariView, animated: true, completion: nil)
-    }
-    
-    @objc
-    private func backButtonClicked(_ sender: UIButton) {
-        self.navigationController?.popViewController(animated: true)
     }
     
     /// 1:1 문의 메일을 보내는 메서드
@@ -171,130 +201,99 @@ extension SettingVC {
     
     /// LoginSB의 루트 네비게이션 컨트롤러로 화면전환하는 메서드
     private func presentToSignNC() {
-        Constants.removeAllUserDefaults()
         guard let signNC = UIStoryboard(name: "Login", bundle: nil).instantiateViewController(withIdentifier: SignNC.className)
                 as? SignNC else { return }
         signNC.modalPresentationStyle = .overFullScreen
         self.present(signNC, animated: true, completion: nil)
     }
+    
+    @objc
+    private func backButtonClicked(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension SettingVC: UITableViewDataSource {
+    
+    // MARK: numberOfSections
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return SettingSection.allCases.count
+    }
+    
+    // MARK: numberOfRowsInSection
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch SettingSection.allCases[section] {
+
+        case .accessAllow, .info:
+            return 1
+        case .account:
+            return 3
+        case .serviceCenter:
+            return 2
+        case .terms:
+            return 6
+        }
+    }
+    
+    // MARK: cellForRowAt
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTVC.identifier) as? SettingTVC else { return UITableViewCell() }
+        
+        switch SettingSection.allCases[indexPath.section] {
+            
+        case .accessAllow:
+            cell.setData(model: SettingSection.accessAllow.settingModel[indexPath.row])
+        case .account:
+            cell.setData(model: SettingSection.account.settingModel[indexPath.row])
+        case .info:
+            cell.setData(model: SettingSection.info.settingModel[indexPath.row])
+        case .serviceCenter:
+            cell.setData(model: SettingSection.serviceCenter.settingModel[indexPath.row])
+        case .terms:
+            cell.setData(model: SettingSection.terms.settingModel[indexPath.row])
+        }
+        
+        cell.settingDelegate = self
+        return cell
+    }
 }
 
 // MARK: - UITableViewDelegate
+
 extension SettingVC: UITableViewDelegate {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 5
+    
+    // MARK: heightForRowAt
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
     }
     
+    // MARK: viewForHeaderInSection
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let bottomView = UIView().then {
-            $0.backgroundColor = UIColor.gray20
-        }
-        let titleLabel = UILabel().then {
-            $0.text = ""
-            $0.font = UIFont.notoSansRegularFont(ofSize: 12)
-            $0.textColor = UIColor.gray50
-            $0.textAlignment = .left
-        }
-        let view = UIView().then {
-            $0.backgroundColor = UIColor.white
-        }
-        view.addSubviews([titleLabel, bottomView])
-        
-        titleLabel.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(18)
-            $0.leading.equalToSuperview().offset(19)
-            $0.width.equalTo(150)
-        }
-        
-        bottomView.snp.makeConstraints {
-            $0.top.leading.trailing.equalToSuperview().offset(0)
-            $0.height.equalTo(1)
-        }
-        
-        switch section {
-        case 0:
-            titleLabel.text = "접근허용"
-            bottomView.backgroundColor = UIColor.white
-        case 1:
-            titleLabel.text = "계정"
-        case 2:
-            titleLabel.text = "정보"
-        case 3:
-            titleLabel.text = "고객센터"
-        default:
-            titleLabel.text = "약관"
-        }
-        return view
+        return configureHeaderView(section: section)
     }
     
+    // MARK: heightForHeaderInSection
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 40
     }
     
+    // MARK: heightForFooterInSection
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         return 1
     }
     
+    // MARK: viewForFooterInSection
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
         let view = UIView().then {
             $0.backgroundColor = UIColor.white
         }
         return view
     }
-}
-
-// MARK: - UITableViewDataSource
-extension SettingVC: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-        case 1:
-            return 3
-        case 3:
-            return 2
-        case 4:
-            return 6
-        default:
-            return 1
-        }
-    }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTVC.identifier) as? SettingTVC else {return UITableViewCell()}
-        var settingData = settingDataModel()
-        
-        switch indexPath.section {
-            //0 접근허용
-        case 0:
-            setPhotoSwitchStatus(cell.toggle)
-            settingData = permissionModel[0]
-            //1 계정
-        case 1:
-            settingData = accountModel[indexPath.row]
-            //2 정보
-        case 2:
-            settingData = infoInquiryModel[0]
-            //3 고객센터
-        case 3:
-            settingData = infoInquiryModel[indexPath.row + 1]
-            //4 약관
-        case 4:
-            settingData = termsModel[indexPath.row]
-        default:
-            return UITableViewCell()
-            
-        }
-        
-        cell.settingDelegate = self
-        cell.setData(isToggle: settingData.isToggle,
-                     toggleData: settingData.toggleData,
-                     isSubLabel: settingData.isSubLabel,
-                     subLabelString: settingData.subLabelString,
-                     titleString: settingData.titleString,
-                     titleLabelColor: settingData.titleLabelColor,
-                     subLabelColor: settingData.subLabelColor)
-        return cell
-    }
-    
+    // MARK: didSelectRowAt
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch indexPath.section {
         case 1:
@@ -346,13 +345,10 @@ extension SettingVC: UITableViewDataSource {
             break
         }
     }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60
-    }
 }
 
 // MARK: - Network
+
 extension SettingVC {
     
     /// 회원 탈퇴
@@ -375,6 +371,7 @@ extension SettingVC {
 }
 
 // MARK: - MFMailComposeViewControllerDelegate
+
 extension SettingVC: MFMailComposeViewControllerDelegate {
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         controller.dismiss(animated: true, completion: nil)
@@ -382,6 +379,7 @@ extension SettingVC: MFMailComposeViewControllerDelegate {
 }
 
 // MARK: - SettingSwitchDelegate
+
 extension SettingVC: SettingSwitchDelegate {
     func switchAction(sender: UISwitch) {
         UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:], completionHandler: nil)
