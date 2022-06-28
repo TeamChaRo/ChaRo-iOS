14d24be5dc26f3f7c6fcdf78fdef6155b5e6e783
Jen <wonee0321@gmail.com>
2022-06-28 23:32:25 +0900

    [FIX] 프로필 수정 파라미터 수정

--- ChaRo-iOS/ChaRo-iOS/Source/Services/UpdateProfileService.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Services/UpdateProfileService.swift
@@ -28,7 +28,7 @@ struct UpdateProfileService {
         let header: HTTPHeaders = ["Content-Type": "multipart/form-data"]
         
         let parameters: [String: Any] = [
-            "originImage": "",
+            "originImage": UserDefaults.standard.object(forKey: Constants.UserDefaultsKey.userImage) ?? "",
             "newNickname": nickname,
         ]
         
