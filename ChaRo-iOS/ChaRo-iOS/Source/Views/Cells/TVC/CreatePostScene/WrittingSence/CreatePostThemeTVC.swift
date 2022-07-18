728a657213996aef01fcae8b2d9b57df01e56284
choinj97@gmail.com <choinj97@gmail.com>
2022-07-19 01:10:43 +0900

    🔨[FIX] : #269 - 테마 선택 select 디자인 반영
    ##### 제목은 최대 50 글자까지만 입력 ############## -> |
    
    # 본문은 위에 작성
    ######## 본문은 한 줄에 최대 72 글자까지만 입력 ########################### -> |
    
    # 꼬릿말은 아래에 작성: ex) #이슈 번호
    
    # --- COMMIT END ---
    #- 🚑️[HOTFIX] : issue나, QA에서 급한 버그 수정에 사용
    #- 🔨[FIX] : 버그, 오류 해결
    #- ➕[ADD] : Feat 이외의 부수적인 코드 추가, 라이브러리 추가, 새로운 파일 생성 시
    #- ✨[FEAT] : 새로운 기능 구현
    #- ⚰️[DEL] : 쓸모없는 코드 삭제
    #- 📝[DOCS] : README나 WIKI 등의 문서 개정
    #- 💄[MOD] : storyboard 파일,UI 수정한 경우
    #- ✅[CHORE] : 코드 수정, 내부 파일 수정
    #- ✏️ [CORRECT] : 주로 문법의 오류나 타입의 변경, 이름 변경 등에 사용합니다.
    #- 🚚[MOVE] : 프로젝트 내 파일이나 코드의 이동
    #- ⏪️[RENAME] : 파일 이름 변경이 있을 때 사용합니다.
    #- ⚡️[IMPROVE] : 향상이 있을 때 사용합니다.
    #- ♻️[REFACTOR] : 전면 수정이 있을 때 사용합니다
    #- 🔀[MERGE]: 다른브렌치를 merge 할 때 사용합니다.
    # ------------------
    #     제목 첫 글자를 대문자로
    #     제목은 명령문으로
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------

--- ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/CreatePostScene/WrittingSence/CreatePostThemeTVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/Cells/TVC/CreatePostScene/WrittingSence/CreatePostThemeTVC.swift
@@ -115,10 +115,33 @@ extension CreatePostThemeTVC {
     }
     
     func configureThemeData(themeList: [String]) {
-        if themeList.isEmpty == false {
+        guard themeList.isEmpty == false else { return }
+        if themeList[0].count > 0 && themeList[0] != "선택안함" {
             self.themeFirstField.text = themeList[0]
+            self.themeFirstField.textColor = .mainBlue
+            self.themeFirstButton.setImage(ImageLiterals.icThemeSelected, for: .normal)
+        } else {
+            self.themeFirstField.text = "테마 1"
+            self.themeFirstField.textColor = .gray40
+            self.themeFirstButton.setImage(ImageLiterals.icUnselect, for: .normal)
+        }
+        if themeList[1].count > 0 && themeList[1] != "선택안함" {
             self.themeSecondField.text = themeList[1]
+            self.themeSecondField.textColor = .mainBlue
+            self.themeSecondButton.setImage(ImageLiterals.icThemeSelected, for: .normal)
+        } else {
+            self.themeSecondField.text = "테마 2"
+            self.themeSecondField.textColor = .gray40
+            self.themeSecondButton.setImage(ImageLiterals.icUnselect, for: .normal)
+        }
+        if themeList[2].count > 0 && themeList[2] != "선택안함"{
             self.themeThirdField.text = themeList[2]
+            self.themeThirdField.textColor = .mainBlue
+            self.themeThirdButton.setImage(ImageLiterals.icThemeSelected, for: .normal)
+        } else {
+            self.themeThirdField.text = "테마 3"
+            self.themeThirdField.textColor = .gray40
+            self.themeThirdButton.setImage(ImageLiterals.icUnselect, for: .normal)
         }
     }
 }
