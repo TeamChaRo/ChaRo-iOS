9a70dff8440c3d7a0ec9c27bb55c03c89d94e2c0
hwangJi-dev <melon2228@swu.ac.kr>
2022-08-12 23:54:38 +0900

    ➕ [ADD] #294 : alert case 추가

--- ChaRo-iOS/ChaRo-iOS/Source/Extensions/UIViewController+.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Extensions/UIViewController+.swift
@@ -33,6 +33,28 @@ extension UIViewController {
         self.present(alertViewController, animated: true, completion: completion)
     }
     
+    func makeRequestAlert(title: String,
+                          message: String,
+                          okTitle: String,
+                          okAction: ((UIAlertAction) -> Void)?,
+                          cancelTitle: String,
+                          cancelAction: ((UIAlertAction) -> Void)? = nil,
+                          completion: (() -> Void)? = nil) {
+        
+        let generator = UIImpactFeedbackGenerator(style: .medium)
+        generator.impactOccurred()
+        
+        let alertViewController = UIAlertController(title: title, message: message,
+                                                    preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: cancelTitle, style: .default, handler: cancelAction)
+        alertViewController.addAction(cancelAction)
+        
+        let okAction = UIAlertAction(title: okTitle, style: .default, handler: okAction)
+        alertViewController.addAction(okAction)
+        
+        self.present(alertViewController, animated: true, completion: completion)
+    }
     
     func makeAlert(title: String,
                    message: String,
