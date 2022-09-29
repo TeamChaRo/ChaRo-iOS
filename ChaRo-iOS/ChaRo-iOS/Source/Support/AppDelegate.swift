676106cc44c737fdc8a132620e143775efc67fb5
hwangJi-dev <melon2228@swu.ac.kr>
2022-09-30 00:46:12 +0900

    ⚰️ [DEL] : #304 - Push Notification 관련 코드 제거, Firebase Package 제거

--- ChaRo-iOS/ChaRo-iOS/Source/Support/AppDelegate.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Support/AppDelegate.swift
@@ -8,37 +8,15 @@
 import UIKit
 import GoogleSignIn
 import KakaoSDKCommon
-import Firebase
-import FirebaseMessaging
 import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-    let gcmMessageIDKey = "gcm.message_id"
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        FirebaseApp.configure()
-        Messaging.messaging().delegate = self
-        
-        // [START register_for_notifications]
-        if #available(iOS 10.0, *) {
-            UNUserNotificationCenter.current().delegate = self
-            
-            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-            UNUserNotificationCenter.current().requestAuthorization(
-                options: authOptions,
-                completionHandler: {_, _ in })
-            application.registerForRemoteNotifications()
-        } else {
-            let settings: UIUserNotificationSettings =
-                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-            application.registerUserNotificationSettings(settings)
-            application.registerForRemoteNotifications()
-        }
         
         // [END register_for_notifications]
         KakaoSDK.initSDK(appKey: "fe470f9e3a4348f2b8dad52d2014efcc")
@@ -74,55 +52,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-}
-
-extension AppDelegate : UNUserNotificationCenterDelegate {
-    
-    // 앱이 foreground상태일 때, 알림이 온 경우 어떻게 표시할 것인지 처리
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                willPresent notification: UNNotification,
-                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        let userInfo = notification.request.content.userInfo
-        
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("Message ID: \(messageID)")
-        }
-        
-        print(userInfo)
-        
-        completionHandler([[.alert, .sound]])
-    }
-    
-    // push가 온 경우 처리. 앱을 실행한 적이 있을 때 처리되는 곳
-    func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                didReceive response: UNNotificationResponse,
-                                withCompletionHandler completionHandler: @escaping () -> Void) {
-        
-        // Print message ID.
-        let userInfo = response.notification.request.content.userInfo
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("Message ID: \(messageID)")
-        }
-        
-        print(userInfo)
-        
-        if response.actionIdentifier == UNNotificationDismissActionIdentifier {
-            print ("Message Closed")
-        }
-        else if response.actionIdentifier == UNNotificationDefaultActionIdentifier {
-            print ("푸시 메시지 클릭 했을 때")
-        }
-        completionHandler()
-    }
-}
-
-extension AppDelegate: MessagingDelegate {
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        print("Firebase registration token: \(String(describing: fcmToken))")
-        
-        let dataDict:[String: String] = ["token": fcmToken ?? ""]
-        NotificationCenter.default.post(name: Notification.Name("FCMToken"), object: nil, userInfo: dataDict)
-        UserDefaults.standard.set(fcmToken, forKey: "fcmToken")
     }
 }
