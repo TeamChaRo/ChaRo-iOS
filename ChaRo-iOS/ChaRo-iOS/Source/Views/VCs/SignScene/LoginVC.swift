6edf5343dbe54af0d8cb0a26533c2156b411f330
hryeong66 <jhr110326@gmail.com>
2022-10-10 18:42:52 +0900

    ✅[CHORE] #300 - 임시 계정 주석 처리

--- ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SignScene/LoginVC.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Views/VCs/SignScene/LoginVC.swift
@@ -35,7 +35,7 @@ class LoginVC: UIViewController {
         configureTextfieldUI()
         configureNotificationCenter()
         configureDelegate()
-        addTestUserAccount()
+        //addTestUserAccount()
     }
     
     private func addTestUserAccount() {
