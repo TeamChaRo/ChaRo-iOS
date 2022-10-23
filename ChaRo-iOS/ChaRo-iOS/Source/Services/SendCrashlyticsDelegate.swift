484d3cda73e209e1ed55a231851f22c39494087c
hryeong66 <jhr110326@gmail.com>
2022-10-23 21:08:01 +0900

    ✅[CHORE] #310 - 사용하지 않는 디버깅 로그 삭제

--- ChaRo-iOS/ChaRo-iOS/Source/Services/SendCrashlyticsDelegate.swift
+++ ChaRo-iOS/ChaRo-iOS/Source/Services/SendCrashlyticsDelegate.swift
@@ -15,7 +15,6 @@ protocol SendCrashlyticsDelegate {
 extension SendCrashlyticsDelegate {
     func presentCrashAlert(at viewController: UIViewController) {
         let error = NSError(domain: "serverError", code: 500)
-        print("crashlytics error = \(error)")
         viewController.makeAlert(title: "서버 연결 오류",
                                  message: "서버 연결에 오류가 발생했습니다.\n 다시 시도해주시길 바랍니다.",
                                  okAction: { _ in
